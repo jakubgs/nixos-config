633ae8fba773516b6735b4a9fcf18e321cdf4158
Jakub Sokołowski <panswiata@gmail.com>
2019-12-29 17:46:43 +0100

    define user password as a hash, add user group
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- hosts/melchior/users.nix
+++ hosts/melchior/users.nix
@@ -1,8 +1,15 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  secrets = import ./secrets.nix;
+in {
   # Make zsh the default shell
   users.defaultUserShell = pkgs.zsh;
+
+  users.groups.sochan = {
+    gid = 1000;
+    name = "sochan";
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sochan = {
@@ -10,6 +17,8 @@
     createHome = true;
     isNormalUser = true;
     useDefaultShell = true;
+    group = "sochan";
+    hashedPassword = secrets.userHashedPassword;
     extraGroups = [ "wheel" "audio" "video" "disk" "adm" "systemd-journal" ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAIEAotJTAk91kQ4skl7hDT5h5GwC/dWCfDXJiQMTw4QrgpNI7rxLhQbgorvN287bzrVig5xBQloMkkm9qqzOn2cv5L7iit8TT9mcrApDiqWBrb05jCm5cu1lINni/MWn5XfQMnE8YnWtwnW+ncd2EcwS9wVDabrTJPFjFYnMaHbl7Ls= sochan@lilim"
--- secrets.example.nix
+++ secrets.example.nix
@@ -1,4 +1,6 @@
 {
+  # user password
+  userHashedPassword = "CHANGE_ME";
   # user by music.nix to configure MPD
   mpdPassword = "CHANGE_ME";
   # ID of ZeroTier private network
