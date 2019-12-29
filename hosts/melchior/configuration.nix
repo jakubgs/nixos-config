e62313a8d66167011e29e891c12821c0eb017d8d
Jakub Sokołowski <panswiata@gmail.com>
2019-12-29 16:31:50 +0100

    drop useless comments, enable mtr
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- hosts/melchior/configuration.nix
+++ hosts/melchior/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -57,17 +53,13 @@
     smartmontools lsscsi
   ];
 
+  # Security
+  programs.mtr.enable = true;
   programs.zsh.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
