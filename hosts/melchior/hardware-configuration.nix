9ffe4f34121f3fc2bf015800d804b16e632b0579
Jakub Sokołowski <panswiata@gmail.com>
2020-01-12 15:14:19 +0100

    hardware: add usb_backup
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- hosts/melchior/hardware-configuration.nix
+++ hosts/melchior/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "mpt3sas" "xhci_pci" "firewire_ohci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "mpt3sas" "xhci_pci" "firewire_ohci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -18,23 +18,8 @@
       fsType = "zfs";
     };
 
-  fileSystems."/mnt/git" =
-    { device = "DATA/git";
-      fsType = "zfs";
-    };
-
   fileSystems."/mnt/torrent" =
     { device = "MEDIA/torrent";
-      fsType = "zfs";
-    };
-
-  fileSystems."/mnt/usb_backup" =
-    { device = "/dev/disk/by-uuid/bef2843c-7e60-4250-aeef-9f8e48427d4f";
-      fsType = "ext4";
-    };
-
-  fileSystems."/mnt/data" =
-    { device = "DATA/data";
       fsType = "zfs";
     };
 
@@ -43,15 +28,24 @@
       fsType = "zfs";
     };
 
-  fileSystems."/git" =
-    { device = "/mnt/git";
-      fsType = "none";
-      options = [ "bind" ];
+  fileSystems."/mnt/data" =
+    { device = "DATA/data";
+      fsType = "zfs";
     };
 
   fileSystems."/mnt/nextcloud" =
     { device = "DATA/nextcloud";
       fsType = "zfs";
+    };
+
+  fileSystems."/mnt/git" =
+    { device = "DATA/git";
+      fsType = "zfs";
+    };
+
+  fileSystems."/mnt/usb_backup" =
+    { device = "/dev/disk/by-uuid/bef2843c-7e60-4250-aeef-9f8e48427d4f";
+      fsType = "ext4";
     };
 
   swapDevices = [ ];
