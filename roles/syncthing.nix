799f0282d2f00e30981d1ec7f0e4799cbcaec282
Jakub Sokołowski <panswiata@gmail.com>
2020-01-26 14:30:24 +0100

    zerotier: change domain to be magi.vpn
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

---
+++ roles/syncthing.nix
@@ -0,0 +1,40 @@
+{ config, lib, ... }:
+
+
+let
+  syncthingHosts = ["melchior" "arael"];
+  notThisHost = h: h != config.networking.hostName;
+  otherHosts = builtins.filter notThisHost syncthingHosts;
+in {
+  services.syncthing = {
+    enable = true;
+    user = "sochan";
+    group = "sochan";
+    configDir = "/home/sochan/.syncthing/config";
+    dataDir = "/home/sochan/.syncthing";
+    guiAddress = "127.0.0.1:8384";
+    openDefaultPorts = true;
+
+    declarative = {
+      devices = lib.filterAttrs (h: v: notThisHost h) {
+        melchior = {
+          id = "42V5WFK-OBSCQW2-73PWWT6-QMGQXKR-TPTITJL-74FVKZV-MRAUJUW-YSPF5QP";
+          addresses = [ "tcp://melchior.magi.vpn:22000" ];
+        };
+        arael = {
+          id = "3LSSBGT-OCETH6T-XXVBYAL-G5ULPYN-CYEA6QL-J2LYOBI-3EVTOPR-CLJBCQO";
+          addresses = [ "tcp://arael.magi.vpn:22000" ];
+        };
+      };
+      folders = {
+        "/mnt/git"   = { id = "git";   type = "sendreceive"; devices = otherHosts; };
+        "/mnt/data"  = { id = "data";  type = "sendreceive"; devices = otherHosts; };
+        "/mnt/music" = { id = "music"; type = "sendreceive"; devices = otherHosts; };
+      };
+    };
+  };
+
+  # Bump the inotify limit
+  # https://docs.syncthing.net/users/faq.html#inotify-limits
+  boot.kernel.sysctl = { "fs.inotify.max_user_watches" = "204800"; };
+}
--- roles/zerotier.nix
+++ roles/zerotier.nix
@@ -17,10 +17,17 @@ in {
 
   # Hosts Entries
   networking.hosts = {
-    "10.2.2.1"  = [ "melchior.magi" ]; # server
-    "10.2.2.2"  = [ "arael.magi" ];    # cloud
-    "10.2.2.11" = [ "caspair.magi" ];  # desktop
-    "10.2.2.12" = [ "lilim.magi" ];    # laptop
-    "10.2.2.13" = [ "ramiel.magi" ];   # phone
+    "10.2.2.1"  = [ "melchior.magi.vpn" ]; # server
+    "10.2.2.2"  = [ "arael.magi.vpn" ];    # cloud
+    "10.2.2.11" = [ "caspair.magi.vpn" ];  # desktop
+    "10.2.2.12" = [ "lilim.magi.vpn" ];    # laptop
+    "10.2.2.13" = [ "ramiel.magi.vpn" ];   # phone
+  };
+
+  # Search the magi domain
+  networking = {
+    enableIPv6 = false;
+    domain = "magi.vpn";
+    search = [ "magi.vpn" ];
   };
 }
