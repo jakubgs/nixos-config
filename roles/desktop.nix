a171c65a4cb6724396fc31b293be2c028bc1ae72
Jakub Sokołowski <panswiata@gmail.com>
2020-02-01 12:26:04 +0100

    gaming: move 32bit opengl and audio setup for wine
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/desktop.nix
+++ roles/desktop.nix
@@ -20,11 +20,6 @@
     };
   };
 
-  # Enable 32bit OpenGL and PulseAudio
-  hardware.opengl.driSupport32Bit = true;
-  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-  hardware.pulseaudio.support32Bit = true;
-
   # System packages
   environment.systemPackages = with pkgs; [
     # Console
--- roles/gaming.nix
+++ roles/gaming.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }:
 
 {
+  # Enable 32bit OpenGL and PulseAudio
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+
   # Packages required for work
   users.users.sochan.packages = with pkgs; [
     steam lutris
