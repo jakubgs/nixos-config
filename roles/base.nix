7ae528d465f3baf41eaac703bac4ce51e018e584
Jakub Sokołowski <panswiata@gmail.com>
2020-02-07 12:48:49 +0100

    base: use unstable patchelf
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/base.nix
+++ roles/base.nix
@@ -6,7 +6,7 @@
     # utilities
     file zsh bash wget curl manpages sudo pass bc rename
     # building
-    gnumake gcc unrar unzip zip envsubst entr patchelf
+    gnumake gcc unrar unzip zip envsubst entr patchelfUnstable
     # processes
     dtach reptyr pstree killall
     # monitoring
