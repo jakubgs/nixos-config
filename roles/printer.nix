acc43c82d2db97d4b44eaaef475c8d9c9871d920
Jakub Sokołowski <panswiata@gmail.com>
2020-02-12 23:24:41 +0100

    printer: configure HL-2130 printer with brlaser driver
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

---
+++ roles/printer.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  # Enable CUPS to print documents.
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ brlaser ];
+  };
+
+  # Define printers
+  hardware.printers.ensurePrinters = [
+    {
+      name = "HL2130";
+      description = "Brother HL-2130";
+      deviceUri = "ipp://printer/ipp/port1";
+      model = "drv:///brlaser.drv/br2140.ppd";
+      ppdOptions = { PageSize = "A4"; };
+    }
+  ];
+
+  # User packages
+  users.users.sochan.packages = with pkgs; [
+    a2ps enscript
+  ];
+}
