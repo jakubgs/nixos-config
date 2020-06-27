6c7d73a765e23076337d042b18dfe48840ad9cf3
Jakub Sokołowski <panswiata@gmail.com>
2020-06-28 01:49:07 +0200

    melchior: fix console arguments
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- hosts/melchior/configuration.nix
+++ hosts/melchior/configuration.nix
@@ -63,9 +63,11 @@
 
   # Select internationalisation properties.
   i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
+  };
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
   };
 
   # Set your time zone.
