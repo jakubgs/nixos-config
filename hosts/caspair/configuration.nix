f3e538d9462c335de80710d6f8ddebae8e871002
Jakub Sokołowski <panswiata@gmail.com>
2020-07-09 23:58:05 +0200

    caspair: switch to 20.03
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- hosts/caspair/configuration.nix
+++ hosts/caspair/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -86,5 +82,5 @@
 
   # Determines the NixOS release with which your system is to be compatible
   # You should change this only after NixOS release notes say you should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "20.03"; # Did you read the comment?
 }
