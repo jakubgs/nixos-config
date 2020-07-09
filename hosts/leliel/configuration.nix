231f1532f7a4f46bc27c4d908f096e87176d001c
Jakub Sokołowski <panswiata@gmail.com>
2020-07-10 00:13:17 +0200

    leliel: add hardware-configuration.nix
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- hosts/leliel/configuration.nix
+++ hosts/leliel/configuration.nix
@@ -26,18 +26,6 @@
   hardware.enableRedistributableFirmware = true;
 
   # Filesystems
-  fileSystems = {
-      # There is no U-Boot on the Pi 4 (yet) -- the firmware partition has to be mounted as /boot.
-      "/boot" = {
-          device = "/dev/disk/by-label/FIRMWARE";
-          fsType = "vfat";
-      };
-      "/" = {
-          device = "/dev/disk/by-label/NIXOS_SD";
-          fsType = "ext4";
-      };
-  };
-
   swapDevices = [ { device = "/swapfile"; size = 1024; } ];
 
   # Networking
