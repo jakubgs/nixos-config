5c32041d6fe3ada6e96cbe4a406d856881610f6a
Jakub Sokołowski <panswiata@gmail.com>
2020-07-15 14:31:35 +0200

    docker: increase devicemapper base image size to 20GB
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/docker.nix
+++ roles/docker.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }:
 
 {
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    extraOptions = "--storage-opt dm.basesize=20G";
+  };
 
   environment.systemPackages = with pkgs; [ docker-compose ];
 }
