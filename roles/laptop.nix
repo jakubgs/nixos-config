9387214df91ebcdddb6ff242a53417a31d1b99dd
Jakub Sokołowski <jakub@status.im>
2020-09-06 18:38:29 +0200

    laptop: enable TLP daemon to improve power management
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/laptop.nix
+++ roles/laptop.nix
@@ -4,6 +4,15 @@
   # allow brightness control by video group
   hardware.acpilight.enable = true;
 
+  # TLP power management daemon
+  services.tlp = {
+    enable = true;
+    extraConfig = ''
+      CPU_SCALING_GOVERNOR_ON_AC=performance
+      CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    '';
+  };
+
   environment.systemPackages = with pkgs; [
     xorg.xbacklight
   ];
