0753fb2b5d0cdab7ab5c8c192700d0e1370b6fb7
Jakub Sokołowski <panswiata@gmail.com>
2020-10-27 09:54:59 +0100

    docker: add net.ipv4.ip_forward=1 for Docker
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/docker.nix
+++ roles/docker.nix
@@ -7,5 +7,10 @@
     extraOptions = "--storage-opt dm.basesize=30G";
   };
 
+  # Fixes: 'IPv4 forwarding is disabled. Networking will not work.'
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+  };
+
   environment.systemPackages = with pkgs; [ docker-compose ];
 }
