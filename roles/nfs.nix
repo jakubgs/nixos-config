38762792feb2fd9efc0bae47acef868a5c7838f0
Jakub Sokołowski <panswiata@gmail.com>
2020-11-15 13:39:06 +0100

    nfs: refactor to template config, make /mnt/backup optional
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/nfs.nix
+++ roles/nfs.nix
@@ -1,6 +1,25 @@
-{ pkgs, ... }:
+{ config, lib, ... }:
 
-{
+let
+  join = lib.concatStringsSep " ";
+  fileSystems = [
+    "/mnt/git"
+    "/mnt/data"
+    "/mnt/music"
+    "/mnt/photos"
+    "/mnt/torrent"
+  ] ++ lib.optionals (config.networking.hostName == "melchior") [
+    "/mnt/backup"
+  ];
+  allowIpRanges = [
+    "10.2.2.0/8"    # Zerotier VPNC
+    "192.168.1.0/8" # Local Network
+  ];
+  # Tempalte NFS config
+  fsExports = map (fs: ''
+    ${fs} ${join (map (r: "${r}(rw,no_subtree_check)") allowIpRanges)}
+  '') fileSystems;
+in {
   # Firewall
   networking.firewall.allowedTCPPorts = [ 111 2049 4000 4001 4002 ];
   networking.firewall.allowedUDPPorts = [ 111 2049 4000 4001 4002 ];
@@ -12,13 +31,6 @@
     lockdPort = 4001;
     mountdPort = 4002;
     # exported shares
-    exports = ''
-      /mnt/git       10.2.2.0/8(rw,no_subtree_check) 192.168.1.0/8(rw,no_subtree_check)
-      /mnt/data      10.2.2.0/8(rw,no_subtree_check) 192.168.1.0/8(rw,no_subtree_check)
-      /mnt/music     10.2.2.0/8(rw,no_subtree_check) 192.168.1.0/8(rw,no_subtree_check)
-      /mnt/photos    10.2.2.0/8(rw,no_subtree_check) 192.168.1.0/8(rw,no_subtree_check)
-      /mnt/backup    10.2.2.0/8(rw,no_subtree_check) 192.168.1.0/8(rw,no_subtree_check)
-      /mnt/torrent   10.2.2.0/8(rw,no_subtree_check) 192.168.1.0/8(rw,no_subtree_check)
-    '';
+    exports = lib.concatStringsSep "" fsExports;
   };
 }
