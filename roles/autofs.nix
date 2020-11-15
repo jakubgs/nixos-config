76a3cfa905bd063f33f2fc88da80ed3cd0837749
Jakub Sokołowski <panswiata@gmail.com>
2020-11-15 14:47:36 +0100

    autofs: mount leliel shares
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/autofs.nix
+++ roles/autofs.nix
@@ -6,7 +6,7 @@
     timeout = 60;
     autoMaster =
       let
-        melchiorConf = pkgs.writeText "melchior" ''
+        melchiorConf = pkgs.writeText "autofs-melchior" ''
           git     -fstype=nfs 192.168.1.12:/mnt/git
           data    -fstype=nfs 192.168.1.12:/mnt/data
           music   -fstype=nfs 192.168.1.12:/mnt/music
@@ -14,8 +14,16 @@
           backup  -fstype=nfs 192.168.1.12:/mnt/backup
           torrent -fstype=nfs 192.168.1.12:/mnt/torrent
         '';
+        lelielConf = pkgs.writeText "autofs-leliel" ''
+          git     -fstype=nfs leliel.magi.vpn:/mnt/git
+          data    -fstype=nfs leliel.magi.vpn:/mnt/data
+          music   -fstype=nfs leliel.magi.vpn:/mnt/music
+          photos  -fstype=nfs leliel.magi.vpn:/mnt/photos
+          torrent -fstype=nfs leliel.magi.vpn:/mnt/torrent
+        '';
       in ''
         /mnt/melchior ${melchiorConf}
+        /mnt/leliel ${lelielConf}
       '';
   };
 }
