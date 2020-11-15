4aafcb55571526568cbf42532844584f831adce1
Jakub Sokołowski <panswiata@gmail.com>
2020-11-15 14:52:01 +0100

    autofs: refactor config generation
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/autofs.nix
+++ roles/autofs.nix
@@ -1,26 +1,25 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
-{
+let
+  genHostConfig = ip: paths: lib.concatStringsSep "\n" (
+    map (p: "${baseNameOf p} -fstype=nfs ${ip}:${p}") paths
+  );
+in {
   services.autofs = {
     enable = true;
     timeout = 60;
     autoMaster =
       let
-        melchiorConf = pkgs.writeText "autofs-melchior" ''
-          git     -fstype=nfs 192.168.1.12:/mnt/git
-          data    -fstype=nfs 192.168.1.12:/mnt/data
-          music   -fstype=nfs 192.168.1.12:/mnt/music
-          photos  -fstype=nfs 192.168.1.12:/mnt/photos
-          backup  -fstype=nfs 192.168.1.12:/mnt/backup
-          torrent -fstype=nfs 192.168.1.12:/mnt/torrent
-        '';
-        lelielConf = pkgs.writeText "autofs-leliel" ''
-          git     -fstype=nfs leliel.magi.vpn:/mnt/git
-          data    -fstype=nfs leliel.magi.vpn:/mnt/data
-          music   -fstype=nfs leliel.magi.vpn:/mnt/music
-          photos  -fstype=nfs leliel.magi.vpn:/mnt/photos
-          torrent -fstype=nfs leliel.magi.vpn:/mnt/torrent
-        '';
+        melchiorConf = pkgs.writeText "autofs-melchior" (
+          genHostConfig "192.168.1.12" [
+            "git" "data" "music" "photos" "backup" "torrent"
+          ]
+        );
+        lelielConf = pkgs.writeText "autofs-leliel" (
+          genHostConfig "leliel.magi.vpn" [
+            "git" "data" "music" "photos" "torrent"
+          ]
+        );
       in ''
         /mnt/melchior ${melchiorConf}
         /mnt/leliel ${lelielConf}
