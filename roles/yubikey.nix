6389cfeb0f77f5625162591fe615c4840588b4ba
Jakub Sokołowski <panswiata@gmail.com>
2020-12-05 21:18:12 +0100

    security: install some related packages
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/yubikey.nix
+++ roles/yubikey.nix
@@ -2,11 +2,21 @@
 
 /* Details: https://nixos.wiki/wiki/Yubikey */
 {
+  imports = [
+    ./security.nix
+  ];
+
+  /* Required tools and libraries. */
+  environment.systemPackages = with pkgs; [
+    ccid opensc pcsctools
+  ];
+
   /* Required udev rules for YubiKey usage */
   services.udev.packages = with pkgs; [
     yubikey-personalization
     libu2f-host
   ];
 
+  /* Necessary for GPG Agent. */
   services.pcscd.enable = true;
 }
