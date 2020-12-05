126a9c75c4de291ed19f90eadd4c253ec552fdf9
Jakub Sokołowski <panswiata@gmail.com>
2020-12-05 21:17:14 +0100

    security: set pinentryFlavor to gnome3
    Signed-off-by: Jakub Sokołowski <panswiata@gmail.com>

--- roles/security.nix
+++ roles/security.nix
@@ -1,18 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  # Increase SSH security
+  /* Increase SSH security. */
   services.openssh = {
     enable = true;
     openFirewall = true;
     passwordAuthentication = false;
     permitRootLogin = "no";
   };
-  # Enable GnuPG agent for keys
+
+  /* Enable GnuPG agent for keys. */
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+    enableBrowserSocket = true;
+    pinentryFlavor = "gnome3";
   };
-  # Use PAM with SSH auth
+
+  /* Use PAM with SSH auth. */
   security.pam.enableSSHAgentAuth = true;
 }
