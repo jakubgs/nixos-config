83481d05b4740ec2d2827fd8605dcee98bf2a629
Jakub Sokołowski <jakub@status.im>
2020-12-15 01:29:49 +0100

    laptop: update format of services.tlp config
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/laptop.nix
+++ roles/laptop.nix
@@ -7,11 +7,11 @@
   # TLP power management daemon
   services.tlp = {
     enable = true;
-    extraConfig = ''
-      CPU_SCALING_GOVERNOR_ON_AC=performance
-      CPU_SCALING_GOVERNOR_ON_BAT=powersave
-      START_CHARGE_THRESH_BAT0=75
-    '';
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      START_CHARGE_THRESH_BAT0 = 75;
+    };
   };
 
   environment.systemPackages = with pkgs; [
