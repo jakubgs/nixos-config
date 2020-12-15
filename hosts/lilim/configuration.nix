853cc6f0d2fa6ef53651be94d6b12fb3fdc1dbc2
Jakub Sokołowski <jakub@status.im>
2020-12-15 02:24:10 +0100

    lilim: upgrade stateVersion to 20.09
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/lilim/configuration.nix
+++ hosts/lilim/configuration.nix
@@ -67,5 +67,5 @@
 
   # Determines the NixOS release with which your system is to be compatible
   # You should change this only after NixOS release notes say you should.
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 }
