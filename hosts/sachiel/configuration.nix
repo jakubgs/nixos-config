a4558d6532ed117eb1ac7058dc8d4ad4b926fc55
Jakub Sokołowski <jakub@status.im>
2021-01-31 18:10:10 +0100

    hosts/sachiel: add new config for NanoPC-T4
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ hosts/sachiel/configuration.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, lib, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../roles/security.nix
+    ../../roles/base.nix
+    ../../roles/zfs.nix
+    ../../roles/users.nix
+    ../../roles/locate.nix
+    ../../roles/netdata.nix
+    ../../roles/wireless.nix
+    ../../roles/zerotier.nix
+    ../../roles/landing.nix
+  ];
+
+  # Boot
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+  # Latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Reboot after 5 seconds on kernel panic
+  boot.kernel.sysctl = { "kernel.panic" = 5; };
+
+  # Enable additional firmware (such as Wi-Fi drivers).
+  hardware.enableRedistributableFirmware = true;
+
+  # Lower power usage and heat generation
+  powerManagement.cpuFreqGovernor = "ondemand";
+
+  # Networking
+  networking = {
+    hostName = "sachiel";
+    hostId = "3cd72605";
+    useDHCP = true;
+  };
+
+  # Miscellaneous
+  time.timeZone = "Europe/Warsaw";
+
+  # High-DPI console
+  console.font = lib.mkDefault "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+
+  # Enable sound.
+  sound.enable = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "20.09";
+}
