7d60fc2e8b2fb97a08096c18b11bb945a20bed59
Jakub Sokołowski <jakub@status.im>
2021-02-15 00:35:50 +0100

    distbuild: add missing user
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/distbuild.nix
+++ roles/distbuild.nix
@@ -7,6 +7,7 @@
       hostName = "caspair.magi.vpn";
       systems = ["x86_64-linux" "aarch64-linux"];
       sshKey = "/home/sochan/.ssh/builder/id_rsa";
+      sshUser = "builder";
       maxJobs = 1;
       speedFactor = 2;
       supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
