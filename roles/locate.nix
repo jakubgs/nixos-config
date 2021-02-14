cfc0a30bd20108464453afb80c7771d2ac313e56
Jakub Sokołowski <jakub@status.im>
2021-02-14 20:43:02 +0100

    locate: adjust pruned paths
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/locate.nix
+++ roles/locate.nix
@@ -5,6 +5,6 @@
   services.locate.enable = true;
   services.locate.locate = pkgs.mlocate;
   services.locate.interval = "daily";
-  services.locate.prunePaths = [ "/mnt/media/torrent/misc" ];
+  services.locate.prunePaths = [ "/mnt/torrent" ];
   services.locate.localuser = null; # Fix user warning
 }
