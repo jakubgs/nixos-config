7c106e3c1dfc0071dcd0296063c2820bb10c9fbc
Jakub Sokołowski <jakub@status.im>
2021-02-15 13:38:15 +0100

    sachiel: drop unnecessary kernel config values
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/sachiel/configuration.nix
+++ hosts/sachiel/configuration.nix
@@ -20,6 +20,23 @@
   boot.loader.grub.enable = false;
   # Enables the generation of /boot/extlinux/extlinux.conf
   boot.loader.generic-extlinux-compatible.enable = true;
+  # Fix for not detecting the NVMe SSD
+  boot.kernelPackages = pkgs.linuxPackages_5_10;
+  boot.kernelPatches = [{
+    name = "pcie-rockchip-config.patch";
+    patch = null;
+    extraConfig = ''
+      NVME_CORE y
+      BLK_DEV_NVME y
+      NVME_MULTIPATH y
+      PCIE_ROCKCHIP y
+      PCIE_ROCKCHIP_EP y
+      PCIE_ROCKCHIP_HOST y
+      PHY_ROCKCHIP_DP y
+      PHY_ROCKCHIP_PCIE y
+      PHY_ROCKCHIP_USB y
+    '';
+  }];
 
   # Reboot after 5 seconds on kernel panic
   boot.kernel.sysctl = { "kernel.panic" = 5; };
