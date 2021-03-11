71528968e9c608bb400f3b2427c0413775904f79
Jakub Sokołowski <jakub@status.im>
2021-03-11 11:23:02 +0100

    clipmenu: specify CM_IGNORE_WINDOW="fpass"
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/clipmenu.nix
+++ roles/clipmenu.nix
@@ -8,4 +8,13 @@ in {
     enable = true;
     package = unstablePkgs.clipmenu;
   };
+
+  # Ignore my script for finding secrets
+  systemd.user.services.clipmenu = {
+    environment = {
+      CM_DEBUG = "0";
+      CM_MAX_CLIPS = "1000";
+      CM_IGNORE_WINDOW = "fpass";
+    };
+  };
 }
