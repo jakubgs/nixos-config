d3e1af9e5b5750d80c3eb6ef55410a7a37acadcb
Jakub Sokołowski <jakub@status.im>
2021-04-07 00:19:02 +0200

    hosts: make indempotent by using fetchurl from pkgs
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/hosts.nix
+++ roles/hosts.nix
@@ -1,20 +1,18 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 let
-  hostsUrls = [
-    https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-    https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn-social/hosts
-  ];
-  hostsFiles = map builtins.fetchurl hostsUrls;
-  hostsContents = map lib.readFile hostsFiles;
+  baseUrl = "https://raw.githubusercontent.com/StevenBlack/hosts";
+  commit = "358526ed7866d474c9158cb61f47c8aabedb8014";
+  hostsFile = pkgs.fetchurl {
+    url = "${baseUrl}/${commit}/alternates/fakenews-gambling-porn-social/hosts";
+    sha256 = "1r408ynygh7vs6flh80h5k2214wzx6q8bmkrik9haidxa23x6fgs";
+  };
+  hostsContent = lib.readFile hostsFile;
 in {
-  networking.extraHosts = lib.concatStringsSep "\n" (
-    hostsContents ++ [
-    ''
-      0.0.0.0 4chan.org
-      0.0.0.0 4channel.org
-      0.0.0.0 boards.4channel.org
-      0.0.0.0 4cdn.org
-    ''
-  ]);
+  networking.extraHosts = hostsContent + ''
+    0.0.0.0 4chan.org
+    0.0.0.0 4channel.org
+    0.0.0.0 boards.4channel.org
+    0.0.0.0 4cdn.org
+  '';
 }
