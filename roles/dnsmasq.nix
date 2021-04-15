e1068d6842100ae7239efd9dc66a608538eefc77
Jakub Sokołowski <jakub@status.im>
2021-04-15 21:53:28 +0200

    dnsmasq: upgrade StevenBlack/hosts to 3.6.3
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/dnsmasq.nix
+++ roles/dnsmasq.nix
@@ -3,7 +3,7 @@
 let
   # Lists for blocking ads, trackers, malware and other garbage.
   noTrackUrl = "https://github.com/notracking/hosts-blocklists/raw/dfc5d3e1bc16c687a0e2a1d6bdf023cfa89574ec/";
-  stevenUrl = "https://github.com/StevenBlack/hosts/raw/358526ed7866d474c9158cb61f47c8aabedb8014/";
+  stevenUrl = "https://github.com/StevenBlack/hosts/raw/3.6.3/";
   fetch = url: sha256: pkgs.fetchurl { inherit url sha256; };
 in {
   services.dnsmasq = {
@@ -19,7 +19,7 @@ in {
 
       conf-file=${fetch "${noTrackUrl}/domains.txt" "1xj4bipyy2crfc4dkhqnvfsfc2j6779n9gfqxnssr3xnza9mcrlc"}
       addn-hosts=${fetch "${noTrackUrl}/hostnames.txt" "004s4lk1an2gbklnc37rsxwqw6dh2d30x109kdzd99d6k05yyiwf"}
-      addn-hosts=${fetch "${stevenUrl}/alternates/fakenews-gambling-porn-social/hosts" "1r408ynygh7vs6flh80h5k2214wzx6q8bmkrik9haidxa23x6fgs"}
+      addn-hosts=${fetch "${stevenUrl}/alternates/fakenews-gambling-porn-social/hosts" "14qvj3pywa6zq2nvsjll1c8kzjh8sdfaz0wflrm3wk10sv9nfvd1"}
     '';
   };
 }
