7ad264a302b3a656fc10581c03b95ba6c78bbfe9
Jakub Sokołowski <jakub@status.im>
2021-04-19 00:59:20 +0200

    pkgs/blocked-fqdns.nix: formatting
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/blocked-fqdns.nix
+++ pkgs/blocked-fqdns.nix
@@ -1,21 +1,25 @@
 { pkgs ? import <nixpkgs> { }
 , excludes ? [ "linkedin.com" ] }:
 
-pkgs.stdenv.mkDerivation {
+let
+  inherit (pkgs) fetchurl;
+  noTracking = "https://github.com/notracking/hosts-blocklists/raw/dfc5d3e1";
+  stevenBlack = "https://github.com/StevenBlack/hosts/raw/3.6.3";
+in pkgs.stdenv.mkDerivation {
   name = "dnsmasq-blocked-fqdns";
 
   # Lists for blocking ads, trackers, malware and other garbage.
-  src = pkgs.fetchurl {
-    url = "https://github.com/notracking/hosts-blocklists/raw/dfc5d3e1bc16c687a0e2a1d6bdf023cfa89574ec/domains.txt";
+  src = fetchurl {
+    url = "${noTracking}/domains.txt";
     sha256 = "1xj4bipyy2crfc4dkhqnvfsfc2j6779n9gfqxnssr3xnza9mcrlc";
   };
   srcs = [
-    (pkgs.fetchurl { 
-      url = "https://github.com/notracking/hosts-blocklists/raw/dfc5d3e1bc16c687a0e2a1d6bdf023cfa89574ec/hostnames.txt";
+    (fetchurl { 
+      url = "${noTracking}/hostnames.txt";
       sha256 = "004s4lk1an2gbklnc37rsxwqw6dh2d30x109kdzd99d6k05yyiwf";
     })
-    (pkgs.fetchurl { 
-      url = "https://github.com/StevenBlack/hosts/raw/3.6.3/alternates/fakenews-gambling-porn-social/hosts";
+    (fetchurl { 
+      url = "${stevenBlack}/alternates/fakenews-gambling-porn-social/hosts";
       sha256 = "14qvj3pywa6zq2nvsjll1c8kzjh8sdfaz0wflrm3wk10sv9nfvd1";
     })
   ];
@@ -23,9 +27,15 @@ pkgs.stdenv.mkDerivation {
   builder = pkgs.writeScript "dnsmasq-blocked-fqdns.sh" ''
     source $stdenv/setup
     mkdir $out
-    cat $src | grep -v -e '^\s*#' > $out/domains
-    cat $srcs | grep -v -e '^\s*#' -e '^::' \
+
+    cat $src \
+      | grep -v -e '^\s*#' \
+      > $out/domains
+
+    cat $srcs \
+      | grep -e '0.0.0.0' \
       | grep -v ${pkgs.lib.concatStringsSep " -e " excludes} \
-      | sort -u > $out/hosts
+      | sort -u \
+      > $out/hosts
   '';
 }
