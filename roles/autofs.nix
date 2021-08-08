5001872a5ad9447979c6f74e9afd1eb949632b24
Jakub Sokołowski <jakub@status.im>
2021-08-08 14:40:18 +0200

    autofs: rafactor to define shares in more generic way
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/autofs.nix
+++ roles/autofs.nix
@@ -1,36 +1,27 @@
 { lib, pkgs, ... }:
 
 let
+  hostShares = {
+    "melchior.magi.vpn" = [ "git" "data" "music" "photos" "torrent" "backup" ];
+    "sachiel.magi.vpn" =  [ "git" "data" "music" "photos" "torrent" ];
+    "leliel.magi.vpn" =   [ "git" "data" "music" "photos" "torrent" ];
+  };
+
   defaultOptions = "async,noac,soft,rsize=262144,wsize=262144";
-  genHostConfig = ip: paths:
+  genNfsShare = addr: paths:
     lib.concatStringsSep "\n" (
-      map (p: "${p} -fstype=nfs,${defaultOptions} ${ip}:/mnt/${p}") paths
+      map (p: "${p} -fstype=nfs,${defaultOptions} ${addr}:/mnt/${p}") paths
     );
+  genShareConfigFile = addr: genFunc:
+    pkgs.writeText "autofs-${addr}" (genFunc addr (builtins.getAttr addr hostShares));
 in {
   services.autofs = {
     enable = true;
     timeout = 60;
-    autoMaster =
-      let
-        melchior = pkgs.writeText "autofs-melchior" (
-          genHostConfig "melchior.magi.vpn" [
-            "git" "data" "music" "photos" "torrent" "backup"
-          ]
-        );
-        leliel = pkgs.writeText "autofs-leliel" (
-          genHostConfig "leliel.magi.vpn" [
-            "git" "data" "music" "photos" "torrent"
-          ]
-        );
-        sachiel = pkgs.writeText "autofs-sachiel" (
-          genHostConfig "sachiel.magi.vpn" [
-            "git" "data" "music" "photos" "torrent"
-          ]
-        );
-      in ''
-        /nfs/melchior ${melchior} --timeout 3
-        /nfs/sachiel  ${sachiel}  --timeout 3
-        /nfs/leliel   ${leliel}   --timeout 3
-      '';
+    autoMaster = ''
+      /nfs/melchior ${genShareConfigFile "melchior.magi.vpn" genNfsShare} --timeout 3
+      /nfs/sachiel  ${genShareConfigFile "sachiel.magi.vpn"  genNfsShare} --timeout 3
+      /nfs/leliel   ${genShareConfigFile "leliel.magi.vpn"   genNfsShare} --timeout 3
+    '';
   };
 }
