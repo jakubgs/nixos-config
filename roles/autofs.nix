3e52b869357ea9dd3ebba4d35b0e23dd700bf3be
Jakub Sokołowski <jakub@status.im>
2021-08-08 15:11:35 +0200

    autofs: add mounting CIFS shares
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/autofs.nix
+++ roles/autofs.nix
@@ -7,21 +7,33 @@ let
     "leliel.magi.vpn" =   [ "git" "data" "music" "photos" "torrent" ];
   };
 
-  defaultOptions = "async,noac,soft,rsize=262144,wsize=262144";
+  defaultNfsOptions = "async,noac,soft,rsize=262144,wsize=262144";
   genNfsShare = addr: paths:
     lib.concatStringsSep "\n" (
-      map (p: "${p} -fstype=nfs,${defaultOptions} ${addr}:/mnt/${p}") paths
+      map (p: "${p} -fstype=nfs,${defaultNfsOptions} ${addr}:/mnt/${p}") paths
     );
+
+  defaultCifsOptions = "rw,credentials=/home/sochan/.smbcredentials";
+  genCifsShare = addr: paths:
+    lib.concatStringsSep "\n" (
+      map (p: "${p} -fstype=cifs,${defaultCifsOptions} ://${addr}/${p}") paths
+    );
+
   genShareConfigFile = addr: genFunc:
     pkgs.writeText "autofs-${addr}" (genFunc addr (builtins.getAttr addr hostShares));
 in {
+  environment.systemPackages = with pkgs; [ cifs-utils ];
+
   services.autofs = {
     enable = true;
     timeout = 60;
     autoMaster = ''
-      /nfs/melchior ${genShareConfigFile "melchior.magi.vpn" genNfsShare} --timeout 3
-      /nfs/sachiel  ${genShareConfigFile "sachiel.magi.vpn"  genNfsShare} --timeout 3
-      /nfs/leliel   ${genShareConfigFile "leliel.magi.vpn"   genNfsShare} --timeout 3
+      /nfs/melchior  ${genShareConfigFile "melchior.magi.vpn" genNfsShare}  --timeout 3
+      /nfs/sachiel   ${genShareConfigFile "sachiel.magi.vpn"  genNfsShare}  --timeout 3
+      /nfs/leliel    ${genShareConfigFile "leliel.magi.vpn"   genNfsShare}  --timeout 3
+      /cifs/melchior ${genShareConfigFile "melchior.magi.vpn" genCifsShare} --timeout 3
+      /cifs/sachiel  ${genShareConfigFile "sachiel.magi.vpn"  genCifsShare} --timeout 3
+      /cifs/leliel   ${genShareConfigFile "leliel.magi.vpn"   genCifsShare} --timeout 3
     '';
   };
 }
