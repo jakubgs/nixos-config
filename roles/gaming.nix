5da86c0714f13e1c182ce964aa74a2821e3b6504
Jakub Sokołowski <jakub@status.im>
2021-08-24 14:20:22 +0200

    gaming: enable ratbagd for mouse configuration
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/gaming.nix
+++ roles/gaming.nix
@@ -6,15 +6,20 @@
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   hardware.pulseaudio.support32Bit = true;
 
+  # Gaming mouse configuration
+  services.ratbagd.enable = true;
+
   # Packages required for work
   users.users.sochan.packages = with pkgs; [
-    wineWowPackages.full
-    vulkan-tools
     steam
     lutris
-    winetricks
     corefonts
-    xboxdrv
+    vulkan-tools
+    # Wine
+    wineWowPackages.full
+    winetricks
+    # Mouse
+    piper
     # Games
     #dwarf-fortress-packages.dwarf-fortress-full
   ];
