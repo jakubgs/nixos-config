da3a1750c3c504d2443b5098ab40be0ebecebee8
Jakub Sokołowski <jakub@status.im>
2021-08-29 20:29:48 +0200

    autofs: refactor to generate config
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/autofs.nix
+++ roles/autofs.nix
@@ -2,40 +2,38 @@
 
 let
   hostShares = {
-    "melchior.magi.vpn" = [ "git" "data" "music" "photos" "torrent" "backup" ];
-    "bardiel.magi.vpn" =  [ "git" "data" "music" "photos" "torrent" ];
-    "sachiel.magi.vpn" =  [ "git" "data" "music" "photos" "torrent" ];
-    "leliel.magi.vpn" =   [ "git" "data" "music" "photos" "torrent" ];
+    #"leliel" =   [ "git" "data" "music" "photos" ];
+    "bardiel" =  [ "git" "data" "music" "photos" ];
+    "sachiel" =  [ "git" "data" "music" "photos" "torrent" ];
+    #"melchior" = [ "git" "data" "music" "photos" "torrent" "backup" ];
   };
 
   defaultNfsOptions = "async,noac,soft,rsize=262144,wsize=262144";
-  genNfsShare = addr: paths:
+  genNfsShare = host: paths:
     lib.concatStringsSep "\n" (
-      map (p: "${p} -fstype=nfs,${defaultNfsOptions} ${addr}:/mnt/${p}") paths
+      map (p: "${p} -fstype=nfs,${defaultNfsOptions} ${host}.magi.vpn:/mnt/${p}") paths
     );
 
-  defaultCifsOptions = "rw,credentials=/home/jakubgs/.smbcredentials";
-  genCifsShare = addr: paths:
+  defaultCifsOptions = "uid=1000,gid=1000,rw,credentials=/home/jakubgs/.smbcredentials";
+  genCifsShare = host: paths:
     lib.concatStringsSep "\n" (
-      map (p: "${p} -fstype=cifs,${defaultCifsOptions} ://${addr}/${p}") paths
+      map (p: "${p} -fstype=cifs,${defaultCifsOptions} ://${host}.magi.vpn/${p}") paths
     );
 
-  genShareConfigFile = addr: genFunc:
-    pkgs.writeText "autofs-${addr}" (genFunc addr (builtins.getAttr addr hostShares));
+  genShareConfigFile = host: genFunc:
+    pkgs.writeText "autofs-${host}" (genFunc host (builtins.getAttr host hostShares));
 in {
   environment.systemPackages = with pkgs; [ cifs-utils ];
 
   services.autofs = {
     enable = true;
     timeout = 60;
-    autoMaster = ''
-      #/nfs/melchior  ${genShareConfigFile "melchior.magi.vpn" genNfsShare}  --timeout 3
-      #/nfs/leliel    ${genShareConfigFile "leliel.magi.vpn"   genNfsShare}  --timeout 3
-      /nfs/sachiel    ${genShareConfigFile "sachiel.magi.vpn"  genNfsShare}  --timeout 3
-      /nfs/bardiel    ${genShareConfigFile "bardiel.magi.vpn"  genNfsShare}  --timeout 3
-      #/cifs/melchior ${genShareConfigFile "melchior.magi.vpn" genCifsShare} --timeout 3
-      #/cifs/leliel   ${genShareConfigFile "leliel.magi.vpn"   genCifsShare} --timeout 3
-      /cifs/sachiel  ${genShareConfigFile "sachiel.magi.vpn"  genCifsShare} --timeout 3
-    '';
+    autoMaster = 
+      lib.concatStrings (lib.mapAttrsToList (host: shares: ''
+        /nfs/${host}  ${genShareConfigFile host genNfsShare}  --timeout 3
+      '') hostShares) +
+      lib.concatStrings (lib.mapAttrsToList (host: shares: ''
+        /cifs/${host} ${genShareConfigFile host genCifsShare} --timeout 3
+      '') hostShares);
   };
 }
