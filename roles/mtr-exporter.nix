b197d2c913b73672f22abf2f72c4c8a1db17a351
Jakub Sokołowski <jakub@status.im>
2021-11-14 21:00:58 +0100

    mtr-exporter: listen on all addresses
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/mtr-exporter.nix
+++ roles/mtr-exporter.nix
@@ -12,6 +12,7 @@
     enable = true;
     target = "vectra.pl";
     interval = 30;
+    address = "0.0.0.0";
     package = pkgs.callPackage ../pkgs/mtr-exporter.nix { };
     mtrPackage = pkgs.mtr.overrideAttrs (old: {
       buildInputs = old.buildInputs ++ [ pkgs.jansson ];
