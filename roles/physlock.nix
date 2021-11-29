5a34cce1be7bb5c1ad0683beaac7388a1ecd3f7a
Jakub Sokołowski <jakub@status.im>
2021-11-29 11:21:42 +0100

    physlock: add lock wrapper script to fix xautolock
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/physlock.nix
+++ roles/physlock.nix
@@ -1,16 +1,27 @@
 { pkgs, ... }:
 
-{
+let
+  lock = pkgs.writeScript "lock" ''
+    exec sudo systemctl start physlock
+  '';
+in {
   # Lock other things too
   services.physlock = {
     enable = true;
     allowAnyUser = true;
+    lockOn = {
+      suspend = true;
+      hibernate = true;
+    };
   };
 
   # Automatically lock after 10 minutes
   services.xserver.xautolock = {
     enable = true;
-    locker = "${pkgs.slock}/bin/slock";
+    locker = "${lock}";
     time = 10;
   };
+
+  environment.interactiveShellInit = "alias lock=${lock}";
+  programs.zsh.interactiveShellInit = "alias lock=${lock}";
 }
