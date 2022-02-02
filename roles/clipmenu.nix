cc02e02eb869df58be2f26f1acd35ec8f1c30c74
Jakub Sokołowski <jakub@status.im>
2022-02-02 02:31:56 +0100

    clipmenu: bump CM_MAX_CLIPS to 2000
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/clipmenu.nix
+++ roles/clipmenu.nix
@@ -8,7 +8,7 @@
   systemd.user.services.clipmenu = {
     environment = {
       CM_DEBUG = "0";
-      CM_MAX_CLIPS = "1000";
+      CM_MAX_CLIPS = "2000";
       CM_IGNORE_WINDOW = "fpass";
     };
   };
