d3a1cc70673bfe6b16625c514bac794d1a3a52df
Jakub Sokołowski <jakub@status.im>
2022-02-08 00:49:15 +0100

    secrets: improve formatting, inherit tools from lib
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/secrets.nix
+++ roles/secrets.nix
@@ -4,6 +4,8 @@
 # https://elvishjerricco.github.io/2018/06/24/secure-declarative-key-management.html
 
 let
+  inherit (lib) assertMsg isString removeSuffix attrByPath;
+
   # builtins.exec expects stdout to be a Nix expression.
   sudoPass = pkgs.writeScript "pass" ''
     sudo -u jakubgs pass "$@" | awk '{ print "\""$0"\""}'
@@ -19,14 +21,16 @@ let
   # FIXME: Without exec flake builds don't see uncommited files.
   overrideSecrets = builtins.exec [ catWrap "/etc/nixos/secrets.nix" ];
 
-  # Helper function for querying pass for secrets in Nix.
-  # First checks secrets.nix file for the given path.
+  # Helper function for querying for secrets in Nix.
+  # 1. Checks secrets.nix file overrides for path.
+  # 2. Calls pass command for given path.
   secretFunc = path:
-    assert (lib.assertMsg (lib.isString path)
-      "Secret path has to be a string!");
-    assert (lib.assertMsg (path != "") "Secret path can't be empty!");
-    let passQuery = lib.removeSuffix "\n" (builtins.exec [ sudoPass path ]);
-    in lib.attrByPath [ path ] passQuery overrideSecrets;
+    assert (assertMsg (isString path) "Secret path has to be a string!");
+    assert (assertMsg (path != "") "Secret path can't be empty!");
+    let
+      queryPass = path: removeSuffix "\n" (builtins.exec [ sudoPass path ]);
+    in
+      attrByPath [ path ] (queryPass path) overrideSecrets;
 in {
   # Allows for use of builtins.exec to call pass.
   # TODO: Replace with nix-plugins in the future.
