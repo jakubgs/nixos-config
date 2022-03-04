30bdc7a067ddee7522d3b3f5e14e9bbf3042642b
Jakub Sokołowski <jakub@status.im>
2022-03-04 18:54:18 +0100

    syncthing: accept all folders by default
    Otherwise it keep asking to accept already accepted folders.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/syncthing.nix
+++ roles/syncthing.nix
@@ -27,26 +27,32 @@ in {
       caspair = { # desktop
         id = "RNHJNYU-IVWMAZT-OL667WV-Y7NOURO-WVT6IHS-MWEBAS6-SDZVQ5C-3MXHYQ5";
         addresses = [ "tcp://caspair.magi.vpn:22000" ];
+        autoAcceptFolders = true;
       };
       melchior = { # server
         id = "42V5WFK-OBSCQW2-73PWWT6-QMGQXKR-TPTITJL-74FVKZV-MRAUJUW-YSPF5QP";
         addresses = [ "tcp://melchior.magi.vpn:22000" ];
+        autoAcceptFolders = true;
       };
       lilim = { # laptop
         id = "IQOAEJV-4CAZQJA-5WZQWPU-WHAWIXD-RNWKSLH-I27MIAX-S4UH2EP-5YDCJQF";
         addresses = [ "tcp://lilim.magi.vpn:22000" ];
+        autoAcceptFolders = true;
       };
       leliel = { # rpi4b
         id = "3JHA3NU-MSQGXDA-H5EB62F-KDCCG7Y-WTJ753S-BXEIT2L-YU3XBIQ-BYLPOQJ";
         addresses = [ "tcp://leliel.magi.vpn:22000" ];
+        autoAcceptFolders = true;
       };
       sachiel = { # nanopct4
         id = "F3AWACC-ALG2TJQ-442WT46-HJ7GGEO-BKXDCSH-5V2RMJJ-DJ42I7A-T7CHEQK";
         addresses = [ "tcp://sachiel.magi.vpn:22000" ];
+        autoAcceptFolders = true;
       };
       bardiel = { # hetzner
         id = "7XUPXBA-DQ7KGZD-VHWO4WI-F37BGFE-4F4NDXK-PD4PUX7-MIJLS6A-6CYA4Q5";
         addresses = [ "tcp://bardiel.magi.vpn:22000" ];
+        autoAcceptFolders = true;
       };
     };
 
