c5673eeae2a54c8a39dcde69583b91fc3b988e06
Jakub Sokołowski <jakub@status.im>
2022-05-25 15:31:32 +0200

    pkgs/gossa: simplify with buildGoPackage
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/gossa.nix
+++ pkgs/gossa.nix
@@ -1,33 +1,14 @@
-{ pkgs ? import <nixpkgs> { }, webui ? pkgs.fetchzip {
-  name = "gossa-ui-a057f08e";
-  url =
-    "https://github.com/pldubouilh/gossa-ui/archive/a057f08e1d9fdbb9556c7f36626cd91f41d267d9.zip";
-  sha256 = "1wc2va74dns3wn1cd5m2chqqiqgs75pczdc8r0zvwzcc5i066fh6";
-} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-pkgs.stdenv.mkDerivation rec {
+pkgs.buildGoPackage rec {
   pname = "gossa";
-  version = "0.2.1";
+  version = "v0.2.1";
   goPackagePath = "github.com/pldubouilh/gossa";
 
-  buildInputs = with pkgs; [ go perl ];
-
-  src = pkgs.fetchzip {
-    url = "https://github.com/pldubouilh/gossa/archive/v${version}.zip";
-    sha256 = "sha256-+XWeaeY3dJtCrZYxliM/58yHRzLuziAqLO58xFIPQ9I=";
+  src = pkgs.fetchgit {
+    url = "https://github.com/pldubouilh/gossa";
+    rev = version;
+    sha256 = "sha256-TT9LtDQNyP9C7Lfb76U+IMR3E5B3pjR0x9UI0GdzqIo=";
+    fetchSubmodules = true;
   };
-
-  configurePhase = ''
-    HOME=$TMPDIR
-    cp -r ${webui}/. gossa-ui/
-  '';
-
-  buildPhase = ''
-    make build
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp gossa $out/bin
-  '';
 }
