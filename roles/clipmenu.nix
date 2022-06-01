236d1ff3ca7d2e3bcc524409b830b732e4894dbc
Jakub Sokołowski <jakub@status.im>
2022-06-01 14:37:45 +0200

    clipmenu: set CM_LAUNCHER=rofi
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/clipmenu.nix
+++ roles/clipmenu.nix
@@ -8,6 +8,7 @@
   systemd.user.services.clipmenu = {
     environment = {
       CM_DEBUG = "0";
+      CM_LAUNCHER = "rofi";
       CM_MAX_CLIPS = "2000";
       CM_IGNORE_WINDOW = "fpass";
     };
