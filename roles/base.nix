663526bebe82bdf84db4273e4c22f0cc61cfae98
Jakub Sokołowski <jakub@status.im>
2022-06-12 12:22:19 +0200

    base: make neovim default editor and add aliases
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/base.nix
+++ roles/base.nix
@@ -41,6 +41,14 @@
   users.defaultUserShell = pkgs.zsh;
 
   # Editor
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
+    defaultEditor = true;
+  };
+
+  # Editor
   environment.variables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
