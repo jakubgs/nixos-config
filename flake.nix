200e0ccc7f3a0215a1c96f7fd7cd673f0c473074
Jakub Sokołowski <jakub@status.im>
2022-06-20 21:59:35 +0200

    flake: move hostnames var to higher let block
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-22.05";
     unstable.url = "nixpkgs/nixos-unstable";
-    hardware.url = github:NixOS/nixos-hardware/master;
+    hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { self, nixpkgs, unstable, hardware }:
@@ -12,20 +12,17 @@
       overlay = final: prev: { unstable = unstable.legacyPackages.${prev.system}; };
       # Overlays-module makes "pkgs.unstable" available in configuration.nix
       overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
+      # To generate host configurations for all hosts.
+      hostnames = builtins.attrNames (builtins.readDir ./hosts);
     in {
-      nixosConfigurations = let
-        # To generate host configurations for all hosts.
-        hostnames = builtins.attrNames (builtins.readDir ./hosts);
-      in builtins.listToAttrs (builtins.map (host:
-        {
-          name = host;
-          value = nixpkgs.lib.nixosSystem {
-            # Simplest way to handle one host running on ARM64.
-            system = if host == "sachiel" then "aarch64-linux" else "x86_64-linux";
-            specialArgs.channels = { inherit nixpkgs unstable hardware; };
-            modules = [ overlayModule ./hosts/${host}/configuration.nix ];
-          };
-        }
-      ) hostnames);
+      nixosConfigurations = builtins.listToAttrs (builtins.map (host: {
+        name = host;
+        value = nixpkgs.lib.nixosSystem {
+          # Simplest way to handle one host running on ARM64.
+          system = if host == "sachiel" then "aarch64-linux" else "x86_64-linux";
+          specialArgs.channels = { inherit nixpkgs unstable hardware; };
+          modules = [ overlayModule ./hosts/${host}/configuration.nix ];
+        };
+      }) hostnames);
     };
 }
