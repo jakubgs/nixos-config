9db4d56aaf9ab85f436b9080a167c7c01ab2ee28
Jakub Sokołowski <jakub@status.im>
2022-07-18 12:24:24 +0200

    roles/prometheus: bump retention from 30 to 120 days
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/prometheus.nix
+++ roles/prometheus.nix
@@ -43,7 +43,7 @@ in {
     port = 9090;
     checkConfig = true;
     extraFlags = [
-      "--storage.tsdb.retention=30d"
+      "--storage.tsdb.retention=120d"
       "--web.external-url=http://${config.networking.fqdn}/prometheus/"
       "--web.route-prefix=/"
     ];
