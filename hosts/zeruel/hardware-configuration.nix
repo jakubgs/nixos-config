29a16b445d1ef005b2c7424ed6a5a9f0c6d4d367
Jakub Sokołowski <jakub@status.im>
2022-08-07 15:11:50 +0200

    zeruel: disable nixpkgs.hostPlatform setting
    Otherwise I get errors like:
    ```
    error: The option `nixpkgs.hostPlatform' does not exist. Definition values:
      - In `/nix/store/zrl9jfcghkrr501xh2nkbzsin9im0k5y-source/hosts/zeruel/hardware-configuration.nix':
        {
          _type = "override";
          content = "x86_64-linux";
          priority = 1000;
        }
    ```
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/zeruel/hardware-configuration.nix
+++ hosts/zeruel/hardware-configuration.nix
@@ -69,7 +69,7 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
