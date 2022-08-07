68620300a69afd41122a1ea9c924299148c6ea47
Jakub Sokołowski <jakub@status.im>
2022-08-07 16:42:00 +0200

    zeruel: add SWAP partitions
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/zeruel/hardware-configuration.nix
+++ hosts/zeruel/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
@@ -61,7 +61,10 @@
       options = [ "noauto" "nofail" ];
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/2d4d3916-cb52-4b87-8c73-819c98026868"; }
+      { device = "/dev/disk/by-uuid/620f2e0f-8d4e-4378-8583-cca502d226ef"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -69,7 +72,6 @@
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
 
-  #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
