3c3d6dedc324a546c245b4739a696263c3fab2ea
Jakub Sokołowski <jakub@status.im>
2022-08-27 16:41:45 +0200

    go-ethereum: upgrade manually to 1.10.23
    https://github.com/ethereum/go-ethereum/releases/tag/v1.10.23
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/go-ethereum.nix
+++ roles/go-ethereum.nix
@@ -3,6 +3,16 @@
 let
   devp2pPort = 9800; # WebDAV Source port
   publicIp = secret "service/geth/public-ip";
+
+  gethPackage = pkgs.unstable.go-ethereum.geth.overrideAttrs (old: rec {
+    version = "1.10.23";
+    src = pkgs.fetchFromGitHub {
+      owner = "ethereum";
+      repo = old.pname;
+      rev = "v${version}";
+      sha256 = "sha256-1fEmtbHKrjuyIVrGr/vTudZ99onkNjEMvyBJt4I8KK4=";
+    };
+  });
 in {
   /* Firewall */
   networking.firewall.allowedTCPPorts = [ devp2pPort ];
@@ -15,7 +25,7 @@ in {
       syncmode = "snap";
       maxpeers = 50;
       port = devp2pPort;
-      package = pkgs.unstable.go-ethereum.geth;
+      package = gethPackage;
       metrics = {
         enable = true;
         port = 16060;
