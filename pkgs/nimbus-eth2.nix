d5e9d1da6e1fb49db8e13b43c2bb673d2558cc6a
Jakub Sokołowski <jakub@status.im>
2022-10-05 15:16:38 +0200

    nimbus-eth2: upgrade to 22.10.0, use new build
    https://github.com/status-im/nimbus-eth2/releases/tag/v22.10.0
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/nimbus-eth2.nix
+++ pkgs/nimbus-eth2.nix
@@ -1,35 +1,37 @@
 {
   pkgs ? import <nixpkgs> { },
+  # Options: nimbus_light_client, nimbus_validator_client, nimbus_signing_node
+  makeTargets ? [ "nimbus_beacon_node" ],
   # WARNING: CPU optmizations that make binary not portable.
-  nativeBuild ? true
+  nativeBuild ? false
 }:
 
 let
-  rev = "f7db5052";
-  fakeGit = pkgs.writeScriptBin "git" "echo ${rev}";
-  fakeLsbRelease = pkgs.writeScriptBin "lsb_release" "echo nix";
-in pkgs.gcc10Stdenv.mkDerivation rec {
-  pname = "nimbus-eth2";
-  version = "22.9.1";
+  inherit (pkgs) stdenv fetchgit lib which writeScriptBin;
+in stdenv.mkDerivation rec {
+  pname = "nimbus";
+  version = "22.10.0";
+  commit = "2d6911d5";
+  name = "${pname}-${version}-${commit}";
 
-  src = pkgs.fetchgit {
-    url = "https://github.com/status-im/${pname}.git";
+  src = fetchgit {
+    url = "https://github.com/status-im/nimbus-eth2.git";
     rev = "v${version}";
-    sha256 = "sha256-oobDVMGrp3Tvc5duIQOebb731LoyW3hbErM16+oo6LI=";
+    sha256 = "sha256-+rkBNop1HDhfdncmlP9UgA6OC4t6SgDhfbb3wZ1INnY=";
     fetchSubmodules = true;
   };
 
-  # Fix for Nim compiler calling 'git rev-parse' and 'lsb_release'.
-  buildInputs = [ fakeGit fakeLsbRelease ];
-  nativeBuildInputs = with pkgs; [ which ];
+  buildInputs = let
+    # Fix for Nim compiler calling 'git rev-parse' and 'lsb_release'.
+    fakeGit = writeScriptBin "git" "echo $commit";
+    fakeLsbRelease = writeScriptBin "lsb_release" "echo nix";
+  in [ fakeGit fakeLsbRelease which ];
 
   enableParallelBuilding = true;
-  dontStrip = true; # Leave debugging symbols in.
 
-  NIMFLAGS = "--debugger:native"
-    + pkgs.lib.optionalString (!nativeBuild) " -d:disableMarchNative";
+  NIMFLAGS = lib.optionalString (!nativeBuild) " -d:disableMarchNative";
 
-  makeFlags = [ "nimbus_beacon_node" "ncli_db" ];
+  makeFlags = makeTargets;
 
   preBuildPhases = [ "buildCompiler" ];
 
@@ -58,7 +60,24 @@ in pkgs.gcc10Stdenv.mkDerivation rec {
 
   installPhase = ''
     mkdir -p $out/bin
-    cp build/ncli_db $out/bin
-    cp build/nimbus_beacon_node $out/bin
+    rm build/generate_makefile
+    cp build/* $out/bin
   '';
+
+  meta = with lib; {
+    homepage = "https://nimbus.guide/";
+    downloadPage = "https://github.com/status-im/nimbus-eth2/releases";
+    changelog = "https://github.com/status-im/nimbus-eth2/blob/stable/CHANGELOG.md";
+    description = "Nimbus is a lightweight client for the Ethereum consensus layer";
+    longDescription = ''
+      Nimbus is an extremely efficient consensus layer client implementation.
+      While it's optimised for embedded systems and resource-restricted devices --
+      including Raspberry Pis, its low resource usage also makes it an excellent choice
+      for any server or desktop (where it simply takes up fewer resources).
+    '';
+    branch = "stable";
+    license = with licenses; [ asl20 mit ];
+    maintainers = with maintainers; [ jakubgs ];
+    platforms = with platforms; x86_64 ++ arm ++ aarch64;
+  };
 }
