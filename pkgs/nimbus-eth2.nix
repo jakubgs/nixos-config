00006444b7f1008644833d4d6a167541ecd809f8
Jakub Sokołowski <jakub@status.im>
2022-12-05 21:28:01 +0100

    pkgs/nimbus-eth2: upgrade from 22.10.1 to 22.11.0
    https://github.com/status-im/nimbus-eth2/releases/tag/v22.11.0
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/nimbus-eth2.nix
+++ pkgs/nimbus-eth2.nix
@@ -7,17 +7,17 @@
 }:
 
 let
-  inherit (pkgs) stdenv fetchgit lib which writeScriptBin;
+  inherit (pkgs) stdenv fetchgit lib nim which writeScriptBin;
 in stdenv.mkDerivation rec {
   pname = "nimbus";
-  version = "22.10.1";
-  commit = "97a1cdc4";
+  version = "22.11.0";
+  commit = "356dd4ee";
   name = "${pname}-${version}-${commit}";
 
   src = fetchgit {
     url = "https://github.com/status-im/nimbus-eth2.git";
     rev = "v${version}";
-    sha256 = "sha256-VmRzzYI+Fvvei4NMzlAugI5uMR46mTwDUqGTMjMhoR4=";
+    sha256 = "sha256-vtXhxR8bLbPE5orItiz9dRVa0KrSg4XOQ4fhQfJCYaQ=";
     fetchSubmodules = true;
   };
 
@@ -25,37 +25,20 @@ in stdenv.mkDerivation rec {
     # Fix for Nim compiler calling 'git rev-parse' and 'lsb_release'.
     fakeGit = writeScriptBin "git" "echo $commit";
     fakeLsbRelease = writeScriptBin "lsb_release" "echo nix";
-  in [ fakeGit fakeLsbRelease which ];
+  in [ fakeGit fakeLsbRelease which nim ];
 
   enableParallelBuilding = true;
 
   NIMFLAGS = lib.optionalString (!nativeBuild) " -d:disableMarchNative";
 
-  makeFlags = makeTargets;
+  # WARNING: Version 1.6.10 is known to be stable.
+  makeFlags = makeTargets ++ [ "USE_SYSTEM_NIM=1" ];
 
-  preBuildPhases = [ "buildCompiler" ];
-
-  # Generate vendor/.nimble contents with correct paths.
+  # Generate the nimbus-build-system.paths file.
   configurePhase = ''
-    export EXCLUDED_NIM_PACKAGES=""
-    export NIMBLE_LINK_SCRIPT=$PWD/vendor/nimbus-build-system/scripts/create_nimble_link.sh
-    export NIMBLE_DIR=$PWD/vendor/.nimble
-    export PWD_CMD=$(which pwd)
     patchShebangs scripts > /dev/null
-    patchShebangs $PWD/vendor/nimbus-build-system/scripts > /dev/null
-    for dep_dir in $(find vendor -type d -maxdepth 1); do
-        pushd "$dep_dir" >/dev/null
-        $NIMBLE_LINK_SCRIPT "$dep_dir"
-        popd >/dev/null
-    done
-  '';
-
-  # Nimbus uses it's own specific Nim version bound as a Git submodule.
-  buildCompiler = ''
-    # Necessary for nim cache creation
-    export HOME=$PWD
-    make -j$NIX_BUILD_CORES build-nim
-    export PATH="$PWD/vendor/nimbus-build-system/vendor/Nim/bin:$PATH"
+    patchShebangs vendor/nimbus-build-system/scripts > /dev/null
+    make nimbus-build-system-paths
   '';
 
   installPhase = ''
