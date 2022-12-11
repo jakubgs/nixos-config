1ffe859bf61e099de74d164d735e190d7fc8f9e2
Jakub Sokołowski <jakub@status.im>
2022-12-12 00:00:33 +0100

    dnsmasq: use DNS over TLS with stubby
    https://dnsprivacy.org/dns_privacy_daemon_-_stubby/configuring_stubby/
    https://github.com/getdnsapi/stubby/blob/v0.4.2/stubby.yml.example
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/dnsmasq.nix
+++ roles/dnsmasq.nix
@@ -5,21 +5,43 @@ let
   formatConfig = ip: fqdns: 
     concatStringsSep "\n" (map (fqdn: "address=/${fqdn}/${ip}") fqdns);
 
+  stubbyExample = pkgs.stubby.passthru.settingsExample;
+
   blockedFqdns = pkgs.callPackage ../pkgs/blocked-fqdns.nix { };
 in {
+  # DNS over TLS
+  services.stubby = {
+    enable = true;
+    settings = stubbyExample // {
+      listen_addresses = [ "127.0.0.1:153" ];
+      log_level = "GETDNS_LOG_NOTICE";
+      dnssec_return_status = "GETDNS_EXTENSION_TRUE";
+      upstream_recursive_servers = stubbyExample.upstream_recursive_servers ++ [
+        {
+          address_data = "1.1.1.1";
+          tls_port = 853;
+          tls_auth_name = "cloudflare-dns.com";
+          tls_pubkey_pinset = [
+            { digest = "sha256";
+              value = "MnLdGiqUGYhtyinlrGTC4FZdDyDXv4NOWFGnXW3ur14="; }
+          ];
+        }
+      ];
+    };
+  };
+
+  # DNS Domain Filtering
   services.dnsmasq = {
     enable = true;
     resolveLocalQueries = true;
     extraConfig = ''
       listen-address=127.0.0.1
       interface=lo
-      bogus-priv
       cache-size=10000
       local-ttl=300
 
-      conf-file=${pkgs.dnsmasq}/share/dnsmasq/trust-anchors.conf
-      dnssec-check-unsigned=no
-      dnssec
+      no-resolv
+      server=127.0.0.1#153
 
       conf-dir=/etc/dnsmasq.d/,*.conf
       conf-file=${blockedFqdns}/domains
