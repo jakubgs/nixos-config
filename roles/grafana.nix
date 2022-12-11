1ce9e237e454f8c7ced610e57b71557488b5dc92
Jakub Sokołowski <jakub@status.im>
2022-12-11 14:27:28 +0100

    grafana: update settigns, datasources
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/grafana.nix
+++ roles/grafana.nix
@@ -5,31 +5,34 @@ let
 in {
   services.grafana = {
     enable = true;
-    protocol = "http";
-    addr = "127.0.0.1";
-    port = 3000;
-    domain = config.networking.fqdn;
-    rootUrl = "%(protocol)s://%(domain)s:%(http_port)s/grafana/";
-    extraOptions = {
-      SERVER_SERVE_FROM_SUB_PATH = "true";
-      SECURITY_ALLOW_EMBEDDING = "true";
-      AUTH_BASIC_ENABLED = "true";
-    };
 
-    security = {
-      adminUser = "jakubgs";
-      adminPassword = secret "service/grafana/pass";
+    settings = {
+      server = {
+        protocol = "http";
+        http_addr = "127.0.0.1";
+        http_port = 3000;
+        domain = config.networking.fqdn;
+        root_url = "%(protocol)s://%(domain)s:%(http_port)s/grafana/";
+      };
+      security = {
+        admin_user = "jakubgs";
+        admin_password = secret "service/grafana/pass";
+        serve_from_sub_path = true;
+        allow_embedding = true;
+      };
     };
 
     provision = {
       enable = true;
-      datasources = lib.optionals (config.services.prometheus.enable) [{
-        name = "localhost";
-        type = "prometheus";
-        url = "http://localhost:${toString config.services.prometheus.port}/";
-        isDefault = true;
-        jsonData = { timeInterval = "10s"; };
-      }];
+      datasources.settings = {
+        datasources = lib.optionals (config.services.prometheus.enable) [{
+          name = "localhost";
+          type = "prometheus";
+          url = "http://localhost:${toString config.services.prometheus.port}/";
+          isDefault = true;
+          jsonData = { timeInterval = "10s"; };
+        }];
+      };
     };
   };
 
@@ -38,7 +41,7 @@ in {
       name = "/grafana/";
       title = "Grafana";
       value = {
-        proxyPass = "http://localhost:${toString services.grafana.port}/";
+        proxyPass = "http://localhost:${toString services.grafana.settings.server.http_port}/";
       };
     }];
   };
