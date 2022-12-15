a8ced149a8c044f8f17f23f6508666b5195becfc
Jakub Sokołowski <jakub@status.im>
2022-12-15 20:41:24 +0100

    pkgs/blocked-fqdns: update all repos
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/blocked-fqdns.nix
+++ pkgs/blocked-fqdns.nix
@@ -3,24 +3,24 @@
 
 let
   inherit (pkgs) fetchurl;
-  noTracking = "https://github.com/notracking/hosts-blocklists/raw/ce5c97a4";
-  stevenBlack = "https://github.com/StevenBlack/hosts/raw/3.11.3";
+  noTracking = "https://github.com/notracking/hosts-blocklists/raw/ad8fd425";
+  stevenBlack = "https://github.com/StevenBlack/hosts/raw/3.11.41";
 in pkgs.stdenv.mkDerivation {
   name = "dnsmasq-blocked-fqdns";
 
   # Lists for blocking ads, trackers, malware and other garbage.
   src = fetchurl {
     url = "${noTracking}/domains.txt";
-    sha256 = "sha256-LRg5f08nbcI5VMzM5rzEyp6NUrB7rnYHckFBxIjrpT8=";
+    sha256 = "sha256-MMYVBANe8YHPUGKg+gM1sfvNyqzk8jZOrcCZbglQHJw=";
   };
   srcs = [
     (fetchurl {
       url = "${noTracking}/hostnames.txt";
-      sha256 = "sha256-UFa03ypTpZtWSBSV9Y0RHwK//5jXfKyefXPcX4mTW3Q=";
+      sha256 = "sha256-u8JtxxgOgotCKfGFxdMj/7SNqRft1CwZVPzdwtz8p5I=";
     })
     (fetchurl {
       url = "${stevenBlack}/alternates/fakenews-gambling-porn-social/hosts";
-      sha256 = "sha256-OxCcZn54ayROZWZPJdadzjZCs7VEhr1RLDta0T67wrc=";
+      sha256 = "sha256-mqeH6nFr/J/w48FqePrmnvRsNsp3hFYbvkCsfyUS75s=";
     })
   ];
 
