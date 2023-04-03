9fc8c82c2953cece874bca0d9645c37f95a25470
Jakub Sokołowski <jakub@status.im>
2023-04-04 00:25:28 +0200

    roles/invidious: enable use_pubsub_feeds
    Subscribe to channels using PubSubHub (Google PubSubHubbub service).
    PubSubHub allows Invidious to be instantly notified when a new video
    is published on any subscribed channels. When PubSubHub is not used,
    Invidious will check for new videos every minute.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/invidious.nix
+++ roles/invidious.nix
@@ -12,6 +12,7 @@ in {
     settings = {
       admins = ["jakubgs"];
       external_port = 80;
+      use_pubsub_feeds = true;
       https_only = false;
       popular_enabled = false;
       quality = "dash";
