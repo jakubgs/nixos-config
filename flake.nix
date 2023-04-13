6e3fb8a397fdf541fcf667f4de106a9cf8cb3b4d
Jakub Sokołowski <jakub@status.im>
2023-04-13 22:31:37 +0200

    flake.nix: add handling for ARM64 hosts
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- flake.nix
+++ flake.nix
@@ -16,12 +16,15 @@
       overlayModule = ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay ]; });
       # To generate host configurations for all hosts.
       hostnames = builtins.attrNames (builtins.readDir ./hosts);
+      # Some hosts are ARM64.
+      systemForHost = hostname:
+        if builtins.elem hostname ["sachiel" "leliel"] then "aarch64-linux"
+        else "x86_64-linux";
     in {
       nixosConfigurations = builtins.listToAttrs (builtins.map (host: {
         name = host;
         value = nixpkgs.lib.nixosSystem {
-          # Simplest way to handle one host running on ARM64.
-          system = if host == "sachiel" then "aarch64-linux" else "x86_64-linux";
+          system = systemForHost host;
           specialArgs.channels = { inherit nixpkgs unstable hardware; };
           modules = [ overlayModule ./hosts/${host}/configuration.nix ];
         };
