3aa715c1b4f5b4630c8ea884cde3832f347db146
Jakub Sokołowski <jakub@status.im>
2023-10-01 13:00:10 +0200

    roles/restic: add more jobs and generic method
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/restic.nix
+++ roles/restic.nix
@@ -1,22 +1,26 @@
 { ... }:
 
-{
+let
+  makeBackup = name: paths: {
+    user = "jakubgs";
+    initialize = true;
+    timerConfig = { OnCalendar = "daily"; Persistent = true; };
+    repository = "sftp:u288137@u288137.your-storagebox.de:/home/${name}";
+    paths = paths;
+    passwordFile = "/home/jakubgs/.usb_backup_pass";
+    pruneOpts = [
+      "--keep-daily 7"
+      "--keep-weekly 3"
+      "--keep-monthly 12"
+      "--keep-yearly 3"
+    ];
+  };
+in {
   services.restic = {
     backups = {
-      "company" = {
-        user = "jakubgs";
-        initialize = true;
-        timerConfig = { OnCalendar = "daily"; Persistent = true; };
-        repository = "sftp:u288137@u288137.your-storagebox.de:/home/company";
-        paths = ["/mnt/data/company"];
-        passwordFile = "/home/jakubgs/.usb_backup_pass";
-        pruneOpts = [
-          "--keep-daily 7"
-          "--keep-weekly 3"
-          "--keep-monthly 12"
-          "--keep-yearly 3"
-        ];
-      };
+      "company"   = makeBackup "company"   ["/mnt/data/company"];
+      "documents" = makeBackup "documents" ["/mnt/data/documents"];
+      "important" = makeBackup "important" ["/mnt/data/important"];
     };
   };
 }
