6991144ac50eed8a3305addddf634d00579e367b
Jakub Sokołowski <jakub@status.im>
2023-10-13 16:47:31 +0200

    roles/syncthing: update ID for ramiel
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/syncthing.nix
+++ roles/syncthing.nix
@@ -55,7 +55,7 @@ in {
         autoAcceptFolders = true;
       };
       ramiel = { # phone
-        id = "ORTBD5P-FZXB5QP-XWY6WP4-IZLASIL-VGSWU5L-7DGMAHF-EH5VG5V-JL3OPAQ";
+        id = "C5M5EGP-5TAPMTV-FY6Y4I6-QGQV27A-TLUWD5A-2JI5YPN-K5WPQZH-PJ527QS";
         addresses = [ "tcp://ramiel.magi.vpn:22000" ];
         autoAcceptFolders = true;
       };
