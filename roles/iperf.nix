4d6b86ef9b79532ba7901eff7d4d574d7be982fe
Jakub Sokołowski <jakub@status.im>
2023-10-14 10:47:30 +0200

    roles/iperf: open port on the firewall
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/iperf.nix
+++ roles/iperf.nix
@@ -1,9 +1,15 @@
-{ ... }:
+{ config, ... }:
 
 {
+  # Service
   services.iperf3 = {
     enable = true;
     openFirewall = true;
     port = 5201;
   };
+
+  # Firewall
+  networking.firewall.allowedTCPPorts = [
+    config.services.iperf3.port
+  ];
 }
