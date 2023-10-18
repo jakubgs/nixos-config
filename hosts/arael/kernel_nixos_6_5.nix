8c4bfdc89f6c6b19fedad3e7c0ab11bd33684dd6
Jakub Sokołowski <jakub@status.im>
2023-10-18 19:54:45 +0200

    hosts/arael: use custom kernel config
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/arael/kernel_nixos_6_5.nix
+++ hosts/arael/kernel_nixos_6_5.nix
@@ -6,11 +6,5 @@ pkgs.linuxPackagesFor (pkgs.linux_6_5.override {
       patch = ./rk3588s_nanopi_r6_dts.patch; }
   ];
 
-  # Enable more debugging.
-  structuredExtraConfig = with lib.kernel; {
-    DEBUG_KERNEL      = yes;
-    DEBUG_LL          = yes;
-    DEBUG_UART_8250   = yes;
-    BACKTRACE_VERBOSE = yes;
-  };
+  configfile = ./kernel.config;
 })
