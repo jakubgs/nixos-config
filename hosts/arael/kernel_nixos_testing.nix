54fcfcd63b03f0392f158c50dfe74f791a4d0005
Jakub Sokołowski <jakub@status.im>
2023-10-20 10:42:31 +0200

    hosts/arael: apply missing RK3588 patches from armbian
    https://github.com/armbian/build/tree/d56478a471790f78045cd0accf2eea7bfaf4420d/patch/kernel/rockchip-rk3588-edge
    https://github.com/jakubgs/nixos-nanopi-r6c/issues/1#issuecomment-1772160390
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/arael/kernel_nixos_testing.nix
+++ hosts/arael/kernel_nixos_testing.nix
@@ -12,10 +12,18 @@ pkgs.linuxPackagesFor (
     configfile = ./kernel.config;
 
     kernelPatches = [
+      { name = "fix-clk-divisions";
+        patch = ./patches/0010-fix-clk-divisions.patch; }
+      { name = "irqchip-fix-its-timeout-issue";
+        patch = ./patches/0011-irqchip-fix-its-timeout-issue.patch; }
+      { name = "RK3588-Add-Cpufreq-Support";
+        patch = ./patches/0023-RK3588-Add-Cpufreq-Support.patch; }
+      { name = "Add-RK3588-USB3-Support";
+        patch = ./patches/0024-Add-RK3588-USB3-Support.patch; }
+      { name = "arm64-dts-rockchip-rk3588-add-sfc-node";
+        patch = ./patches/0025-arm64-dts-rockchip-rk3588-add-sfc-node.patch; }
       { name = "rk3588s_nanopi_r6_dts";
         patch = ./patches/rk3588s_nanopi_r6_dts.patch; }
-      { name = "rk3588s_nanopi_r6_dts_fix";
-        patch = ./patches/rk3588s_nanopi_r6_dts_fix.patch; }
     ];
 
     allowImportFromDerivation = true;
