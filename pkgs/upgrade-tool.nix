4106d2d6f49a9bc65d25b4614c03e5c1db1a1cfc
Jakub Sokołowski <jakub@status.im>
2023-10-21 22:30:02 +0200

    pkgs/upgrade-tool: fix download source
    The old link stopped working.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/upgrade-tool.nix
+++ pkgs/upgrade-tool.nix
@@ -1,25 +1,26 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.stdenv.mkDerivation {
-  pname = "linux-upgrade-tool";
-  version = "1.59";
+  pname = "rockchip-upgrade-tool";
+  version = "1.54";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/rockchip-linux/tools/raw/d386f33f3f1d20af733dbee95dd358ba0edd5c72/linux/Linux_Upgrade_Tool/Linux_Upgrade_Tool/upgrade_tool";
-    sha256 = "1g6ic4qm1dfw84kdm4gg8lhn601w3acafq459j0x7yz7bhn49a9q";
+    url = "https://github.com/rockchip-linux/rkbin/raw/master/tools/upgrade_tool";
+    sha256 = "sha256-Z0iUUpDWPRGbVCueEjoIllEOmOiwpvSsJ5yKUn2mIQI=";
   };
 
   phases = [ "installPhase" "fixupPhase" ];
 
   installPhase = ''
-    cp $src $out
-    chmod +x $out
+    mkdir $out/bin
+    cp $src $out/bin/
+    chmod +x $out/bin/upgrade_tool
   '';
 
-  meta = with pkgs.stdenv.lib; {
+  meta = with pkgs.lib; {
     description = "Rockchip Develop tools for flashing via USB-C";
     longDescription = "For flashing u-boot images to my NanoPC-T4";
-    homepage = "https://github.com/rockchip-linux/tools";
+    homepage = "https://github.com/rockchip-linux/rkbin";
     platforms = platforms.linux;
     maintainers = with maintainers; [ jakubgs ];
   };
