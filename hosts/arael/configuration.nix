1f3a06ba6f51479b418b2388def18021e97f5389
Jakub Sokołowski <jakub@status.im>
2023-10-22 01:36:08 +0200

    hosts/arael: add first working configuation
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ hosts/arael/configuration.nix
@@ -0,0 +1,55 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    efi = {
+      efiSysMountPoint = "/boot";
+      canTouchEfiVariables = true;
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    grub.enable = false;
+  };
+
+  # Get new drivers.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking = {
+    hostName = "arael";
+    hostId = "892cff1c";
+    useDHCP = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "pl";
+  };
+
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      PasswordAuthentication = true;
+      PermitRootLogin = "yes";
+    };
+  }; 
+
+  # Copy the NixOS config and symlink at run/current-system/configuration.nix.
+  system.copySystemConfiguration = true;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
