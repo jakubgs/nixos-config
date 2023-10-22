7e098fd7363d810a209a58976d247795181c18ff
Jakub Sokołowski <jakub@status.im>
2023-10-22 20:14:13 +0200

    roles/restic: name repo after hostname, add tags
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/restic.nix
+++ roles/restic.nix
@@ -1,12 +1,13 @@
-{ ... }:
+{ config, ... }:
 
 let
+  repository = "sftp:u288137@u288137.your-storagebox.de:/home/${config.networking.hostName}";
   makeBackup = name: paths: {
+    inherit repository;
     user = "jakubgs";
     initialize = true;
-    timerConfig = { OnCalendar = "daily"; Persistent = true; };
-    repository = "sftp:u288137@u288137.your-storagebox.de:/home/${name}";
     paths = paths;
+    extraBackupArgs = ["--tag=${name}"];
     passwordFile = "/home/jakubgs/.usb_backup_pass";
     pruneOpts = [
       "--keep-daily 7"
@@ -14,6 +15,11 @@ let
       "--keep-monthly 12"
       "--keep-yearly 3"
     ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Persistent = true;
+      RandomizedDelaySec = "1h";
+    };
   };
 in {
   services.restic = {
