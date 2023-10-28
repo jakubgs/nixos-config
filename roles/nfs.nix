8ea2f96060c71a34b819844bd85fb79a8661dde2
Jakub Sokołowski <jakub@status.im>
2023-10-28 12:19:05 +0200

    roles/nfs: use paths based on if they are mounted
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/nfs.nix
+++ roles/nfs.nix
@@ -2,20 +2,22 @@
 
 let
   join = lib.concatStringsSep " ";
-  fileSystems = [
+  isMounted = path: lib.hasAttr path config.fileSystems;
+
+  fileSystems = lib.filter isMounted [
     "/mnt/git"
     "/mnt/data"
     "/mnt/music"
     "/mnt/photos"
-  ]
-    ++ lib.optionals (config.networking.hostName == "sachiel")  [ "/mnt/torrent" ]
-    ++ lib.optionals (config.networking.hostName == "caspair")  [ "/mnt/torrent" ]
-    ++ lib.optionals (config.networking.hostName == "melchior") [ "/mnt/backup" ];
+    "/mnt/torrent"
+    "/mnt/backup"
+  ];
 
   allowIpRanges = [
     "10.2.2.0/8"    # Zerotier VPNC
     "192.168.1.0/8" # Local Network
   ];
+
   # Tempalte NFS config
   fsExports = map (fs: ''
     ${fs} ${join (map (r: "${r}(rw,no_subtree_check)") allowIpRanges)}
