57ec095349133953b7d3de6d82a75dd8895e43a3
Jakub Sokołowski <jakub@status.im>
2023-11-04 22:15:25 +0100

    hosts/arael: drop disabling UAS for IcyBox
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/arael/configuration.nix
+++ hosts/arael/configuration.nix
@@ -35,9 +35,6 @@
   # Serial console or keyboard is not easily accessible.
   boot.zfs.requestEncryptionCredentials = false;
 
-  # Disable USB Attached Storage(UAS) for IcyBox enclosure.
-  boot.extraModprobeConfig = "options usb-storage quirks=1234:5678:u";
-
   networking = {
     hostName = "arael";
     hostId = "892cff1c";
