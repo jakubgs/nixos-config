49b77c48b21f38b28f14b4fe391d1768dc220f80
Jakub Sokołowski <jakub@status.im>
2023-11-04 23:49:21 +0100

    roles/gitweb: use ConditionPathIsMountPoint and wantedBy
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/gitweb.nix
+++ roles/gitweb.nix
@@ -1,32 +1,49 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Hosting
-  services.nginx = {
-    enable = true;
-    gitweb = {
+  options.gitweb = {
+    repos =   lib.mkOption { default = "/git"; };
+    subpath = lib.mkOption { default = "/gitweb"; };
+  };
+
+  config = let
+    inherit (pkgs.lib) pathToMountUnit;
+    cfg = config.gitweb;
+  in {
+    # Hosting
+    services.nginx = {
       enable = true;
-      location = "/gitweb";
-      virtualHost = "${config.networking.hostName}.${config.networking.domain}";
-    };
-  };
-
-  # Service
-  services.gitweb = {
-    projectroot = "/git";
-    gitwebTheme = true;
-  };
-
-  services.landing = {
-    proxyServices = [{
-      name = "/gitweb/";
-      title = "WebGit";
-      value = {
-        proxyPass = "http://localhost:80/gitweb/";
-        extraConfig = ''
-          proxy_set_header Host default;
-        '';
+      gitweb = {
+        enable = true;
+        location = "${cfg.subpath}";
+        virtualHost = "${config.networking.hostName}.${config.networking.domain}";
       };
-    }];
+    };
+
+    # Service
+    services.gitweb = {
+      projectroot = config.gitweb.repos;
+      gitwebTheme = true;
+    };
+
+    # Wait for mount and start after.
+    systemd.services.gitweb = {
+      after =    [ (pathToMountUnit cfg.repos) ];
+      wantedBy = [ (pathToMountUnit cfg.repos) ];
+      unitConfig.ConditionPathIsMountPoint = cfg.repos;
+    };
+
+    services.landing = {
+      proxyServices = [{
+        name = "${cfg.subpath}/";
+        title = "WebGit";
+        value = {
+          proxyPass = "http://localhost:80${cfg.subpath}/";
+          extraConfig = ''
+            proxy_set_header Host default;
+          '';
+        };
+      }];
+    };
   };
 }
