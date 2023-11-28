1febdbd466e60c334c162e9e6a09258424b802ce
Jakub Sokołowski <jakub@status.im>
2023-11-28 11:20:00 +0100

    pkgs/consul2: upgrade Python to 3.10 to match system
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/consul2.nix
+++ pkgs/consul2.nix
@@ -1,15 +1,15 @@
 { pkgs, lib, ... }:
 
-pkgs.python39Packages.buildPythonPackage rec {
+pkgs.python310Packages.buildPythonPackage rec {
   pname = "python-consul2";
   version = "0.1.5";
 
-  src = pkgs.python39Packages.fetchPypi {
+  src = pkgs.python310Packages.fetchPypi {
     inherit pname version;
     sha256 = "sha256-KchZ3nPhfzarmb6DH9wdkk8W6HciMyd6KO+St7mZlc0=";
   };
 
-  propagatedBuildInputs = with pkgs.python39Packages; [ six requests ];
+  propagatedBuildInputs = with pkgs.python310Packages; [ six requests ];
 
   # Some tests require Consul binaries.
   doCheck = false;
