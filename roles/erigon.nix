824ca02329c15f28191208aa0e0aa61218901b2a
Jakub Sokołowski <jakub@status.im>
2023-12-03 20:37:13 +0100

    roles/erigon: convert to get secrets from agenix
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/erigon.nix
+++ roles/erigon.nix
@@ -1,69 +1,71 @@
 { config, pkgs, lib, secret, ... }:
 
-let
-  devp2pPort = 9800; # WebDAV Source port
-  publicIp = secret "hosts/${config.networking.hostName}/ip";
-in {
-  services.erigon = {
-    enable = true;
-    package = pkgs.callPackage ../pkgs/erigon.nix { };
-    secretJwtPath = "/etc/erigon/jwtsecret";
-    settings = {
-      "chain" = "mainnet";
-      "mine" = true;
-      "miner.etherbase" = secret "service/nimbus/fee-recipient";
-      "allow-insecure-unlock" = true;
-      "maxpeers" = 100;
-      "nat" = "extip:${publicIp}";
-      "datadir" = "/mnt/erigon";
-      "log.console.json" = true;
-      "log.console.verbosity" = "info";
-      # DevP2P
-      "port" = devp2pPort;
-      "p2p.allowed-ports" = [devp2pPort (devp2pPort+1)];
-      # RPC
-      "http.addr" = "localhost";
-      "http.port" = 18545;
-      # AuthRPC / Engine API
-      "authrpc.addr" = "localhost";
-      "authrpc.port" = 18551;
-      "authrpc.vhosts" = "localhost,127.0.0.1,${config.networking.hostName}";
-      # Metrics
-      "metrics" = true;
-      "metrics.addr" = "0.0.0.0";
-      "metrics.port" = 16060;
+{
+  options.erigon = {
+    devp2pPort = lib.mkOption { default = 9800; };
+    jwtSecret  = lib.mkOption { default = secret "service/nimbus/web3-jws-secret"; };
+  };
+
+  config = let
+    cfg = config.erigon;
+  in {
+    age.secrets."service/nimbus/web3-jws-secret" = {
+      file = ../secrets/service/nimbus/web3-jws-secret.age;
+      owner = "nimbus";
     };
-  };
 
-  # Custom user
-  users.groups.erigon.gid = 500;
-  users.users.erigon = {
-    uid = 500;
-    group = "erigon";
-    isSystemUser = true;
-  };
-  systemd.services.erigon = {
-    # Wait for torrent volume to be mounted.
-    after = lib.mkForce [
-      "network.target" (
-        pkgs.lib.pathToMountUnit config.services.erigon.settings.datadir
-      )
-    ];
-    serviceConfig = {
-      User = "erigon";
-      DynamicUser = lib.mkForce false;
+    services.erigon = {
+      enable = true;
+      package = pkgs.callPackage ../pkgs/erigon.nix { };
+      secretJwtPath = cfg.jwtSecret;
+      settings = {
+        "chain" = "mainnet";
+        "mine" = true;
+        "miner.etherbase" = secret "service/nimbus/fee-recipient";
+        "allow-insecure-unlock" = true;
+        "maxpeers" = 100;
+        "datadir" = "/mnt/erigon";
+        "log.console.json" = true;
+        "log.console.verbosity" = "info";
+        # DevP2P
+        "port" = cfg.devp2pPort;
+        "p2p.allowed-ports" = [cfg.devp2pPort (cfg.devp2pPort+1)];
+        # RPC
+        "http.addr" = "localhost";
+        "http.port" = 18545;
+        # AuthRPC / Engine API
+        "authrpc.addr" = "localhost";
+        "authrpc.port" = 18551;
+        "authrpc.vhosts" = "localhost,127.0.0.1,${config.networking.hostName}";
+        # Metrics
+        "metrics" = true;
+        "metrics.addr" = "0.0.0.0";
+        "metrics.port" = 16060;
+      };
     };
+
+    # Custom user
+    users.groups.erigon.gid = 500;
+    users.users.erigon = {
+      uid = 500;
+      group = "erigon";
+      isSystemUser = true;
+    };
+    systemd.services.erigon = {
+      # Wait for torrent volume to be mounted.
+      after = lib.mkForce [
+        "network.target" (
+          pkgs.lib.pathToMountUnit config.services.erigon.settings.datadir
+        )
+      ];
+      serviceConfig = {
+        User = "erigon";
+        DynamicUser = lib.mkForce false;
+      };
+    };
+
+    /* Firewall */
+    networking.firewall.allowedTCPPorts = [ cfg.devp2pPort ];
+    networking.firewall.allowedUDPPorts = [ cfg.devp2pPort ];
   };
-
-  # Secrets
-  environment.etc."erigon/jwtsecret" = {
-    mode = "0440";
-    user = config.systemd.services.erigon.serviceConfig.User;
-    text = secret "service/nimbus/web3-jws-secret";
-  };
-
-  /* Firewall */
-  networking.firewall.allowedTCPPorts = [ devp2pPort ];
-  networking.firewall.allowedUDPPorts = [ devp2pPort ];
-
 }
