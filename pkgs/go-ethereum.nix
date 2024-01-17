5dba05cb502320b652e1064dedc5967321d4cd00
Jakub Sokołowski <jakub@status.im>
2024-01-17 23:31:31 +0300

    pkgs/go-ethereum: upgrade from 1.13.5 to 1.13.10
    https://github.com/ethereum/go-ethereum/releases/tag/v1.13.10
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/go-ethereum.nix
+++ pkgs/go-ethereum.nix
@@ -2,17 +2,17 @@
 
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.go-ethereum.override {
-  buildGoModule = args: pkgs.buildGo120Module ( args // rec {
-    version = "1.13.5";
+  buildGoModule = args: pkgs.buildGo121Module ( args // rec {
+    version = "1.13.10";
 
     src = pkgs.fetchFromGitHub {
       owner = "ethereum";
       repo = args.pname;
       rev = "v${version}";
-      sha256 = "sha256-UbRsY9fSUYAwPcLfGGDHeqvSsLKUKR+2a93jH5xA9uQ=";
+      sha256 = "sha256-HFAf5fjYiRx6OEbqdtgaan8bqZPHazYKWG2Ty+WhNu8=";
     };
 
-    vendorHash = "sha256-dOvpOCMxxmcAaticSLVlro1L4crAVJWyvgx/JZZ7buE=";
+    vendorHash = "sha256-w4WUDLzpr0VTwLK84/Yq4ia3Q1XhxDttzQ5RGg2FQGU=";
 
     subPackages = [
       "cmd/ethkey"
