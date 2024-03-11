4426eb1fa36f2beaed3b40ef03a8b04e668e1dce
Jakub Sokołowski <jakub@status.im>
2024-03-12 00:38:48 +0100

    pkgs/go-ethereum: upgarde from 1.13.13 to 1.13.14
    https://github.com/ethereum/go-ethereum/releases/tag/v1.13.14
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/go-ethereum.nix
+++ pkgs/go-ethereum.nix
@@ -3,13 +3,13 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.go-ethereum.override {
   buildGoModule = args: pkgs.buildGo121Module ( args // rec {
-    version = "1.13.13";
+    version = "1.13.14";
 
     src = pkgs.fetchFromGitHub {
       owner = "ethereum";
       repo = args.pname;
       rev = "v${version}";
-      sha256 = "sha256-MfsRWJU59mGjjzioqmMqKJoZks+XxlALkyOIC/xI1KU=";
+      sha256 = "sha256-+o/yOsS8tm8zvTJ17jd+dNPJpJB0vsjf4WRWNv6HgG0=";
     };
 
     vendorHash = "sha256-LWNFuF66KudxrpWBBXjMbrWP5CwEuPE2h3kGfILIII0=";
