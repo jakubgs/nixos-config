053452ca773c86039086f65205dfa640cdb9dd37
Jakub Sokołowski <jakub@status.im>
2024-07-30 00:10:00 +0200

    pkgs/go-ethereum: upgrade from 1.14.5 to 1.14.7
    https://github.com/ethereum/go-ethereum/releases/tag/v1.14.7
    https://github.com/ethereum/go-ethereum/releases/tag/v1.14.6
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/go-ethereum.nix
+++ pkgs/go-ethereum.nix
@@ -3,16 +3,16 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.go-ethereum.override {
   buildGoModule = args: pkgs.buildGo121Module ( args // rec {
-    version = "1.14.5";
+    version = "1.14.7";
 
     src = pkgs.fetchFromGitHub {
       owner = "ethereum";
       repo = args.pname;
       rev = "v${version}";
-      sha256 = "sha256-IY0BKoDRMVRZTIysdUgqhTFQx0Pz+kl61vbPbhSdT8k=";
+      sha256 = "sha256-axllyeumgR6DJ5NXGBVanLcjfeDEHhBTRI9eiJUpFw4=";
     };
 
-    vendorHash = "sha256-L9kJ3YaUz2iwiI3IDQAWnTc2U9BvUep+fAPENIFhhY4=";
+    vendorHash = "sha256-WN+3TN7opyS9mYSGbxZsDNNAAulaGqVjXNXW4JDRvvQ=";
     /* Fix for: fatal error: os/events_posix.h: No such file */
     proxyVendor = true;
 
