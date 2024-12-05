78eafb3c68b3b110a4592a095112974d74ad4eda
Jakub Sokołowski <jakub@status.im>
2024-12-05 23:56:14 +0100

    hosts/caspair: favor console config from roles/console
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/caspair/configuration.nix
+++ hosts/caspair/configuration.nix
@@ -56,10 +56,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "pl";
-  };
 
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
