0ea9ecb7df48910cc47cb2dfaf14c5626d28e610
Jakub Sokołowski <jakub@status.im>
2024-12-07 12:26:14 +0100

    flakes: upgrade unstable to fix fzf ctrl-r bug
    https://github.com/NixOS/nixpkgs/pull/357123
    https://github.com/junegunn/fzf/issues/1472
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/nix.nix
+++ roles/nix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  nix.package = pkgs.unstable.nixVersions.nix_2_23;
+  nix.package = pkgs.nixStable;
   nix.extraOptions = ''
     extra-experimental-features = flakes nix-command
     debugger-on-trace = true
