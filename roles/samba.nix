392ced2ba039dcfba2ee8f0bcb14a18d2bfc8808
Jakub Sokołowski <jakub@status.im>
2024-12-07 15:57:33 +0100

    roles/samba: drop extraConfig, use settings
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/samba.nix
+++ roles/samba.nix
@@ -7,8 +7,8 @@ let
     name = builtins.baseNameOf path;
     value = {
       inherit path;
-      browseable = "yes";
-      writeable = "no";
+      "browseable" = "yes";
+      "writeable" = "no";
       "guest ok" = "yes";
       "guest only" = "yes";
       "force user" = "nobody";
@@ -18,8 +18,8 @@ let
     name = builtins.baseNameOf path;
     value = {
       inherit path;
-      browseable = "no";
-      writeable = "yes";
+      "browseable" = "no";
+      "writeable" = "yes";
     };
   };
   shares = {
@@ -43,22 +43,25 @@ in {
   networking.firewall.allowedUDPPorts = [ 137 138 ];
 
   # Daemon
-  services.samba.enable = true;
-  services.samba.extraConfig = ''
-    netbios name = ${config.networking.hostName}
-    name resolve order = bcast host
-    load printers = no
-    printcap name = /dev/null
-    printing = bsd
-    guest account = nobody
-    map to guest = Bad User
-    # Avoid ipv6 bind errors
-    bind interfaces only = yes
-  '';
-  services.samba.shares = with lib; (
-    listToAttrs (map makePublicShare shares.public) //
-    listToAttrs (map makePrivateShare shares.private)
-  );
+  services.samba = {
+    enable = true;
+    settings = {
+      "global" = {
+        "netbios name" = config.networking.hostName;
+        "name resolve order" = "bcast host";
+        "load printers" = false;
+        "printcap name" = "/dev/null";
+        "printing" = "bsd";
+        "guest account" = "nobody";
+        "map to guest" = "Bad User";
+        # Avoid ipv6 bind errors
+        "bind interfaces only" = true;
+      };
+    } // (
+      lib.listToAttrs (map makePublicShare shares.public) //
+      lib.listToAttrs (map makePrivateShare shares.private)
+    );
+  };
 
   # User for /mnt/ania
   users.groups.ania = {
