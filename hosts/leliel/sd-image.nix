626a1bd2a357505ee7985a00b719c3a6ec8508a5
Jakub Sokołowski <jakub@status.im>
2024-12-14 11:39:19 +0100

    hosts/leliel: increase default size of boot partition
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/leliel/sd-image.nix
+++ hosts/leliel/sd-image.nix
@@ -5,6 +5,12 @@
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64-installer.nix>
   ];
 
+  # Increase default size of vFAT partition to 1 GB.
+  sdImage.firmwareSize = 1024;
+
+  # bzip2 compression takes loads of time with emulation, skip it.
+  sdImage.compressImage = false;
+
   # Set serial console settings
   boot.kernelParams = ["console=ttyS2,1500000"];
 
@@ -16,9 +22,6 @@
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDeWB4SeXQfEsfkNPOkSLoTQ/7VDpf8CsaRQ+waCHEEv4v2fFc/9lMbQ6Z208UEQKJMOMdtwd3eB7j6aFIirMQTYcm/NuxPLdRRnlxLNJIVMBfKUV5V3OkbneqzBTEvtAaIDC506kIlXxAPfZCDVxzAi7B+NkHUvhjCEjScM2KfamahDZUbj2ww2Q/82P1Qj8QY/1b2wC6OXBnKPUQIzAzrxDNYWaXdB/4DysDcib50kd2URenpMVU1DCjSWXBniSnpEVh0Lxjehsnfg+oE3BP3u6wA+1xufukH9h9eQ/hTM1PXEVC2ObpgESRYxc3rqkqVxYbOzrmCRVJpvKoGs+W89vIoFUt6/tzunAMogo2VHhT7LnGE4iizj9YODxIdpRMGGeMgZiceoOuNFAjKg8Qay4aoE50uklim4ircOXgrAasRotUcz28EU5oaV9/NO+GKNzooRNBX2U/c1MsTI+6mz7ppMq0NCHOpO5sY1qC8F2lZbDDGQgC25btqu+xnbqHwCDSst2Sy5yvF3C34F/Xt8kw3zkraB1OmTWwW/QIA+o3AViaA59r+ZicIIEWvUbUbcMD/GFDesOgzK8V9G6kZNuQoEVsq9FHEMTpsGSBDOIHn4aWP+7gQK2FhvyXBGj/z/NDFY1H+I2KvhI0rkV3NaTtUy0+51uKO5Efnx8cQyw== cardno:9 020 049" # CURRENT
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGUXvuXqZwlLOSxYqthiWF1J5U4E93SUGDu7gk4Ok30T jakubgs@caspair" # NEW
   ];
-
-  # bzip2 compression takes loads of time with emulation, skip it.
-  sdImage.compressImage = false;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.11";
