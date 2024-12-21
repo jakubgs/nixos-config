e37481be4fd951b974fc4835851b4eda022d7a72
Jakub Sokołowski <jakub@status.im>
2024-12-21 15:08:41 +0100

    hosts/iruel: add another Radxa Rock 5B host
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ hosts/iruel/README.md
@@ -0,0 +1,27 @@
+# Description
+
+Configuration for [Radxa Rock 5B](https://radxa.com/rock5b/).
+
+# Hardware
+
+* Rockchip RK3588s
+* Quad Cortex®-A76 @ 2.2~2.4GHz
+* Quad Cortex®-A55 @ 1.8GHz
+* Mali G610MC4 GPU, OpenGL ES1.1, ES2.0, ES3.2
+* 8 GB 64bit LPDDR4x
+* 16MB SPI Flash for bootloader
+* eMMC socket
+* Samsung PM981 512 GB
+
+# Docs
+
+* [Installation Process](../israfel/INSTALL.md)
+* [Debug UART Console](../israfel/UART.md)
+
+# Links
+
+* https://wiki.radxa.com/Rock5/5B
+* https://wiki.radxa.com/Rock5/hardware/5b
+* https://wiki.radxa.com/Rock5/dev/serial-console
+* https://nixos.wiki/wiki/NixOS_on_ARM/Radxa_ROCK5_Model_B
+* https://github.com/jakubgs/nixos-config/issues/4
---
+++ hosts/iruel/configuration.nix
@@ -0,0 +1,51 @@
+{ modulesPath, config, lib, pkgs, ... }:
+
+{
+  imports = [
+    (modulesPath + "/profiles/hardened.nix")
+    ./hardware-configuration.nix
+    ../../roles/base.nix
+    ../../roles/zfs.nix
+    ../../roles/users.nix
+    ../../roles/locate.nix
+    ../../roles/zerotier.nix
+    ../../roles/landing.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    efi = {
+      efiSysMountPoint = "/boot";
+      canTouchEfiVariables = true;
+    };
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 10;
+    };
+    grub.enable = false;
+  };
+
+  # Load thermal sensor module.
+  boot.kernelModules = [ "rockchip-thermal" ];
+  # Lock kernel version.
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  # Fix missing symbols dropped in kernel 6.2.
+  boot.zfs.removeLinuxDRM = true;
+
+  # Serial console or keyboard is not easily accessible.
+  boot.zfs.requestEncryptionCredentials = false;
+
+  networking = {
+    hostId = "b3c24b35";
+    useDHCP = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
