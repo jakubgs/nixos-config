60fc8102fe5fa5f8e049338883e9e0b8ac13bd91
Jakub Sokołowski <jakub@status.im>
2024-12-25 14:38:26 +0100

    files/dotfiles.sh: use pull with rebase and autostash
    Avoids the need to stash using separate commands.
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- files/dotfiles.sh
+++ files/dotfiles.sh
@@ -6,13 +6,13 @@ export DOTFILES_PATH="${HOME}/dotfiles"
 export DOTFILES_URL='@dotfilesUrl@'
 export GIT_SSH_COMMAND='ssh -i @sshKey@ -o StrictHostKeyChecking=accept-new'
 
-if [[ -d "${DOTFILES_PATH}" ]]; then
-    echo "Fetching dotfiles new changes..."
-    cd "${DOTFILES_PATH}"
-    git pull
-else
+if [[ ! -d "${DOTFILES_PATH}" ]]; then
     echo "Cloning dotfiles repository..."
     git clone "${DOTFILES_URL}" "${DOTFILES_PATH}"
+else
+    echo "Fetching dotfiles new changes..."
+    cd "${DOTFILES_PATH}"
+    git pull --rebase --autostash
 fi
 
 echo "Symlinking configuration..."
