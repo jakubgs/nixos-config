417e20ad3ace03e21174c1f7c52cfd50bd1ff50c
Jakub Sokołowski <jakub@status.im>
2025-01-13 20:20:04 +0100

    roles/ollama: add simple LLM service config
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

---
+++ roles/ollama.nix
@@ -0,0 +1,42 @@
+{ lib, config, ... }:
+
+{
+  options.ollama = {
+    apiPort   = lib.mkOption { default = 11434; };
+    webuiPort = lib.mkOption { default = 3000;  };
+  };
+
+  config = let
+    cfg = config.ollama;
+  in {
+    users.users.ollama.uid = 3000;
+    users.groups.ollama.gid = 3000;
+
+    services.ollama = {
+      enable = true;
+      port = cfg.apiPort;
+      user = "ollama";
+      group = "ollama";
+      home = "/var/lib/ollama";
+      acceleration = "cuda";
+      loadModels = [
+        "llama2-uncensored:7b"
+        "codellama:7b"
+        "llava:13b"
+      ];
+    };
+
+    services.open-webui = {
+      enable = true;
+      host = "0.0.0.0";
+      port = cfg.webuiPort;
+      environment = {
+        OLLAMA_API_BASE_URL = "http://localhost:${toString cfg.apiPort}";
+        WEBUI_HOSTNAME = config.networking.fqdn;
+        WEBUI_AUTH = "False";
+      };
+    };
+
+    networking.firewall.interfaces."zt*".allowedTCPPorts = [ cfg.webuiPort ];
+  };
+}
