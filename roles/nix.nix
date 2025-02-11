33b1eddca244906f6ff0de7630c80a4ef9c75007
Jakub Sokołowski <jakub@status.im>
2025-02-11 14:59:00 +0100

    roles/nix: bump download-buffer-size to 256 MB
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/nix.nix
+++ roles/nix.nix
@@ -2,10 +2,11 @@
 
 {
   nix.package = pkgs.nixVersions.nix_2_25;
-  nix.extraOptions = ''
-    extra-experimental-features = flakes nix-command
-    debugger-on-trace = true
-  '';
+  nix.settings = {
+    "download-buffer-size" = "268435456";
+    "extra-experimental-features" = "flakes nix-command";
+    "debugger-on-trace" = true;
+  };
 
   # Lower priority of builds to not Disturb other processes.
   nix.daemonCPUSchedPolicy = "idle";
