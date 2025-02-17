c7a75169ca9cb43a33484eabd30b39cc1a67c35f
Jakub Sokołowski <jakub@status.im>
2025-02-17 13:34:10 +0100

    pkgs/go-ethereum: upgrade from 1.15.1 to 1.15.2
    https://github.com/ethereum/go-ethereum/releases/tag/v1.15.2
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/go-ethereum.nix
+++ pkgs/go-ethereum.nix
@@ -3,13 +3,13 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.go-ethereum.override {
   buildGoModule = args: pkgs.buildGo122Module ( args // rec {
-    version = "1.15.1";
+    version = "1.15.2";
 
     src = pkgs.fetchFromGitHub {
       owner = "ethereum";
       repo = args.pname;
       rev = "v${version}";
-      sha256 = "sha256-xPW2JKez815lGZTHrUfWzuYtMENxysqSzbVu+3KqD60=";
+      sha256 = "sha256-eaUkPl8vQzvotYfZcnuBwphSfO33RPjWOYhyNNvXli4=";
     };
 
     vendorHash = "sha256-cfBTSroeDb/htGzIWG8c9Jty+Qo0TrQBrnyYy/Yo2C4=";
