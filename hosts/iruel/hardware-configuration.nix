9fd0e7688c3c1dc948ede3f04049277cc1b01366
Jakub Sokołowski <jakub@status.im>
2025-03-09 11:58:38 +0100

    hosts/iruel: add USB-DATA zfs configuration
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/iruel/hardware-configuration.nix
+++ hosts/iruel/hardware-configuration.nix
@@ -13,6 +13,43 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
+  fileSystems."/mnt/data" =
+    { device = "USB-DATA/data";
+      fsType = "zfs";
+      options = [ "noauto" "nofail" ];
+    };
+
+  fileSystems."/mnt/git" =
+    { device = "USB-DATA/git";
+      fsType = "zfs";
+      options = [ "noauto" "nofail" ];
+    };
+
+  fileSystems."/mnt/mobile" =
+    { device = "USB-DATA/mobile";
+      fsType = "zfs";
+      options = [ "noauto" "nofail" ];
+    };
+
+  fileSystems."/mnt/music" =
+    { device = "USB-DATA/music";
+      fsType = "zfs";
+      options = [ "noauto" "nofail" ];
+    };
+
+  fileSystems."/mnt/photos" =
+    { device = "USB-DATA/photos";
+      fsType = "zfs";
+      options = [ "noauto" "nofail" ];
+    };
+
+  fileSystems."/mnt/grzegorz" =
+    { device = "USB-DATA/grzegorz";
+      fsType = "zfs";
+      options = [ "noauto" "nofail" ];
+    };
+
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
