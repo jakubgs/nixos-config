7776ea03aaf5d143f5f8b22bd2d70861e8861383
Jakub Sokołowski <jakub@status.im>
2025-03-23 10:09:03 +0100

    roles/syncthing: add folders type for each host
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/syncthing.nix
+++ roles/syncthing.nix
@@ -1,57 +1,85 @@
 { config, lib, pkgs, ... }:
 
 
-{
+let
+  cfg = config.syncthing;
+in {
   options.syncthing = {
     hosts = lib.mkOption {
       default = {
         caspair = { # desktop
-          id = "RNHJNYU-IVWMAZT-OL667WV-Y7NOURO-WVT6IHS-MWEBAS6-SDZVQ5C-3MXHYQ5";
-          addresses = [ "tcp://caspair.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "RNHJNYU-IVWMAZT-OL667WV-Y7NOURO-WVT6IHS-MWEBAS6-SDZVQ5C-3MXHYQ5";
+            addresses = [ "tcp://caspair.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type = "sendreceive"; };
         };
         melchior = { # server
-          id = "42V5WFK-OBSCQW2-73PWWT6-QMGQXKR-TPTITJL-74FVKZV-MRAUJUW-YSPF5QP";
-          addresses = [ "tcp://melchior.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "42V5WFK-OBSCQW2-73PWWT6-QMGQXKR-TPTITJL-74FVKZV-MRAUJUW-YSPF5QP";
+            addresses = [ "tcp://melchior.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "receiveonly"; };
         };
         lilim = { # laptop
-          id = "GGEMIJR-WYRWDE7-4QOMTZF-2QDESUK-264P4HR-3JSC7R5-662VW36-BPEOGAC";
-          addresses = [ "tcp://lilim.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "GGEMIJR-WYRWDE7-4QOMTZF-2QDESUK-264P4HR-3JSC7R5-662VW36-BPEOGAC";
+            addresses = [ "tcp://lilim.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "sendreceive"; };
         };
         leliel = { # rpi4b
-          id = "3JHA3NU-MSQGXDA-H5EB62F-KDCCG7Y-WTJ753S-BXEIT2L-YU3XBIQ-BYLPOQJ";
-          addresses = [ "tcp://leliel.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "3JHA3NU-MSQGXDA-H5EB62F-KDCCG7Y-WTJ753S-BXEIT2L-YU3XBIQ-BYLPOQJ";
+            addresses = [ "tcp://leliel.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "receiveonly"; };
         };
         sachiel = { # nanopct4
-          id = "4R5Z36S-6S5HBSC-2BAEZEH-HCNLXJH-GRDYZ6P-X66AFAK-RDUCRKA-PNX5YQU";
-          addresses = [ "tcp://sachiel.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "4R5Z36S-6S5HBSC-2BAEZEH-HCNLXJH-GRDYZ6P-X66AFAK-RDUCRKA-PNX5YQU";
+            addresses = [ "tcp://sachiel.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "receiveonly"; };
         };
         arael = { # nanopi-r6c
-          id = "HOBTQ6M-WYSYZM3-SVXACFZ-VOLJRR3-XCIWSZB-FLPYML4-F4BE35Q-3NIF4AB";
-          addresses = [ "tcp://arael.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "HOBTQ6M-WYSYZM3-SVXACFZ-VOLJRR3-XCIWSZB-FLPYML4-F4BE35Q-3NIF4AB";
+            addresses = [ "tcp://arael.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "receiveonly"; };
         };
         bardiel = { # hetzner
-          id = "7XUPXBA-DQ7KGZD-VHWO4WI-F37BGFE-4F4NDXK-PD4PUX7-MIJLS6A-6CYA4Q5";
-          addresses = [ "tcp://bardiel.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "7XUPXBA-DQ7KGZD-VHWO4WI-F37BGFE-4F4NDXK-PD4PUX7-MIJLS6A-6CYA4Q5";
+            addresses = [ "tcp://bardiel.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "receiveonly"; };
         };
         ramiel = { # phone
-          id = "2UQXCXB-LZHCGVM-PFOL3NW-GRU57X7-R4IHVR7-SS2WDSP-TX5DFSG-YCNG7QJ";
-          addresses = [ "tcp://ramiel.magi.vpn:22000" ];
-          autoAcceptFolders = true;
+          device = {
+            id = "2UQXCXB-LZHCGVM-PFOL3NW-GRU57X7-R4IHVR7-SS2WDSP-TX5DFSG-YCNG7QJ";
+            addresses = [ "tcp://ramiel.magi.vpn:22000" ];
+            autoAcceptFolders = true;
+          };
+          folder = { type= "sendreceive"; };
         };
       };
     };
   };
 
   config = let
-    notThisHost = h: h != config.networking.hostName;
-    otherHosts = builtins.filter notThisHost (lib.attrNames config.syncthing.hosts);
+    hostname = config.networking.hostName;
+    notThisHost = h: _: h != hostname;
+    otherHosts = lib.filterAttrs notThisHost cfg.hosts;
+    otherHostnames = lib.attrNames otherHosts;
     inherit (config) services;
   in {
     # Firewall
@@ -66,19 +94,23 @@
       configDir = "/home/jakubgs/.syncthing/config";
       dataDir = "/home/jakubgs/.syncthing";
       guiAddress = "127.0.0.1:8384";
+      overrideDevices = true;
+      overrideFolders = true;
 
       # Use when syncing gets stuck.
       #extraFlags = ["--reset-deltas"];
 
       settings = {
-        devices = lib.filterAttrs (h: _: notThisHost h) config.syncthing.hosts;
+        devices = lib.mapAttrs (k: v: v.device) otherHosts;
 
-        folders = {
-          "/mnt/git"    = { id = "git";    type = "sendreceive"; devices = otherHosts; };
-          "/mnt/data"   = { id = "data";   type = "sendreceive"; devices = otherHosts; };
-          "/mnt/music"  = { id = "music";  type = "sendreceive"; devices = otherHosts; };
-          "/mnt/photos" = { id = "photos"; type = "sendreceive"; devices = otherHosts; };
-          "/mnt/mobile" = { id = "mobile"; type = "sendreceive"; devices = otherHosts; };
+        folders = let
+          folderOpts = cfg.hosts."${hostname}".folder;
+        in {
+          "/mnt/git"    = { id = "git";    devices = otherHostnames; } // folderOpts;
+          "/mnt/data"   = { id = "data";   devices = otherHostnames; } // folderOpts;
+          "/mnt/music"  = { id = "music";  devices = otherHostnames; } // folderOpts;
+          "/mnt/photos" = { id = "photos"; devices = otherHostnames; } // folderOpts;
+          "/mnt/mobile" = { id = "mobile"; devices = otherHostnames; } // folderOpts;
         };
       };
     };
