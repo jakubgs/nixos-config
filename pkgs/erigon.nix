041535878b7364b3a79560639ebcb47375845ef6
Jakub Sokołowski <jakub@status.im>
2025-04-05 14:24:34 +0200

    pkgs/erigon: upgrade from 2.61.3 to 3.0.0
    https://github.com/erigontech/erigon/releases/tag/v3.0.0
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/erigon.nix
+++ pkgs/erigon.nix
@@ -3,24 +3,22 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.erigon.override {
   buildGoModule = args: pkgs.buildGo122Module ( args // rec {
-    version = "2.61.3";
+    version = "3.0.0";
 
     src = pkgs.fetchFromGitHub {
       owner = "ledgerwatch";
       repo = args.pname;
       rev = "v${version}";
-      hash = "sha256-VGLuPaGYx/DQc3Oc9wAbELXAtkuxr8cbePVBExlZikk=";
+      hash = "sha256-63wh9D5D9qzABEvKCBxBRHYuGBnlX+hrme56STkyoQU=";
       fetchSubmodules = true;
     };
 
-    vendorHash = "sha256-1LB2T0o9LjFdpl86NPMKx1lFLrQZefAGldcSQyL6O7M=";
+    vendorHash = "sha256-Mvat9+mbgpept9g8pYNf4a/bAGZGBM0MAM417DDIT9w=";
 
     subPackages = [
       "cmd/erigon"
     ];
 
-    # Silkworm is disabled for releases. Fails due to rpath.
-    # Would require 'lib/linux_x64/libsilkworm_capi.so' from silkworm-go.
-    tags = "-tags=nosqlite,noboltdb,nosilkworm";
+    doCheck = false;
   });
 }
