547315c9fec36ec866acd82eae8245473a019441
Jakub Sokołowski <jakub@status.im>
2025-04-21 18:38:26 +0200

    roles/base/nix: upgrade nix from 2.25 to 2.28
    https://discourse.nixos.org/t/nix-2-27-0-released/
    https://discourse.nixos.org/t/nix-2-26-released/
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/base/nix.nix
+++ roles/base/nix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  nix.package = pkgs.nixVersions.nix_2_25;
+  nix.package = pkgs.nixVersions.nix_2_28;
   nix.settings = {
     "download-buffer-size" = "268435456";
     "extra-experimental-features" = "flakes nix-command";
