a5d0d4064a54cd903bd01aac3d98cf22ac3ed183
Jakub Sokołowski <jakub@status.im>
2025-04-26 08:05:24 +0200

    pkgs/go-ethereum: upgrade from 1.15.8 to 1.15.10
    https://github.com/ethereum/go-ethereum/releases/tag/v1.15.10
    https://github.com/ethereum/go-ethereum/releases/tag/v1.15.9
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/go-ethereum.nix
+++ pkgs/go-ethereum.nix
@@ -3,13 +3,13 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.go-ethereum.override {
   buildGoModule = args: pkgs.buildGo123Module ( args // rec {
-    version = "1.15.8";
+    version = "1.15.10";
 
     src = pkgs.fetchFromGitHub {
       owner = "ethereum";
       repo = args.pname;
       rev = "v${version}";
-      sha256 = "sha256-dEGPObm3Hy2MHOYrk+lga0PAeHrdSt0jfsTrk74wCUA=";
+      sha256 = "sha256-QVgi4IDPDGlvg8J3Wwrjtzkd6eYi3aAwU8IJbnIr5fU=";
     };
 
     vendorHash = "sha256-1FuVdx84jvMBo8VO6q+WaFpK3hWn88J7p8vhIDsQHPM=";
