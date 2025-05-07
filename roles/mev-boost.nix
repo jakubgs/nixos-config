8e63dfe706c5474dda06012f06c8536b6e14dde8
Jakub Sokołowski <jakub@status.im>
2025-05-07 09:20:46 +0200

    roles/mev-boost: upgrade from 1.8 to 1.9 for Pectra
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/mev-boost.nix
+++ roles/mev-boost.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ../services/mev-boost.nix ];
@@ -7,6 +7,7 @@
     enable = true;
     logJson = true;
     logLevel = "info";
+    package = pkgs.unstable.mev-boost;
     relays = [
       "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net"
       "https://0xa00001792da52b2ad6606033396337f86e7598c8e0927d640bb649e0a9fe230df41f8ffd3403a5cd4fffd5131e2fbe47@rsync-builder.xyz"
