06b2c6f7dc822d69ca57a66e62a1d9e568a0ee81
Jakub Sokołowski <jakub@status.im>
2025-05-17 19:25:23 +0200

    roles/desktop/crypto: update udev rules for Ledger
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/desktop/crypto.nix
+++ roles/desktop/crypto.nix
@@ -12,6 +12,13 @@
   # Fix permissions
   # https://github.com/LedgerHQ/udev-rules/blob/master/20-hw1.rules
   services.udev.extraRules = ''
-    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001|1011|1015", MODE="0666", GROUP="adm"
+    # HW.1, Nano
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c|2b7c|3b7c|4b7c", TAG+="uaccess", TAG+="udev-acl"
+
+    # Blue, NanoS, Aramis, HW.2, Nano X, NanoSP, Stax, Ledger Test,
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", TAG+="uaccess", TAG+="udev-acl"
+
+    # Same, but with hidraw-based library (instead of libusb)
+    KERNEL=="hidraw*", ATTRS{idVendor}=="2c97", MODE="0666"
   '';
 }
