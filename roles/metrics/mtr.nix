07610a2a3ff707d75bfb77662f7b00519e60539d
Jakub Sokołowski <jakub@status.im>
2025-06-03 17:41:10 +0200

    roles/metrics/mtr: upgrade exporter to 0.5.1 release
    https://github.com/mgumz/mtr-exporter/releases/tag/0.5.1
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/metrics/mtr.nix
+++ roles/metrics/mtr.nix
@@ -29,13 +29,13 @@
     # https://github.com/mgumz/mtr-exporter/pull/13
     # branch: feature/add-labels-first-intermediate
     package = pkgs.unstable.mtr-exporter.overrideAttrs (_: {
-      version = "0.4.0-rc.0";
+      version = "0.5.1";
       vendorHash = null;
       src = pkgs.fetchFromGitHub {
         owner = "mgumz";
         repo = "mtr-exporter";
-        rev = "9cf7a317c7b34fdc20aef1fc8361dd4fab1a43f0";
-        hash = "sha256-Tm6elJkcxh3xM4x2q63wKWrpilK7WEdto7emYNToka0=";
+        rev = "d49969f6ab7d586e966204e7e56a717f0d07b7fb";
+        hash = "sha256-+myQg27TGclU+SfU8oO+DvXYqc/8sWE2zRK6fL2DhwM=";
       };
     });
   };
