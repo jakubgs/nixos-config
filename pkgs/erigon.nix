fdea144f368ed481130bb1956264641757f059d5
Jakub Sokołowski <jakub@status.im>
2025-11-30 11:49:29 +0100

    pkgs/erigon: upgrade from 3.2.0 to 3.3.0
    Necessary for Fusaka upgrade on Mainnet:
    https://github.com/erigontech/erigon/releases/tag/v3.3.0
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/erigon.nix
+++ pkgs/erigon.nix
@@ -3,17 +3,17 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.erigon.override {
   buildGoModule = args: pkgs.buildGo124Module ( args // rec {
-    version = "3.2.0";
+    version = "3.3.0";
 
     src = pkgs.fetchFromGitHub {
       owner = "ledgerwatch";
       repo = args.pname;
       rev = "v${version}";
-      hash = "sha256-PmQRNXHh39OR5XQqBheHUee/ugvK7oPZb4Lfh2Rn30U";
+      hash = "sha256-5h9TxOvEfDbOkVTW6Py97sQRfomgu7U5w+iZvuZLri8=";
       fetchSubmodules = true;
     };
 
-    vendorHash = "sha256-pAHIiRG6v2ARy95+rTZfFNoZ9xs82kvybH2kuWcpD8Q=";
+    vendorHash = "sha256-J/cEUrw9U67+CZmb1woI2HltYyo2v90ap/jiOqwMesY=";
 
     subPackages = [
       "cmd/erigon"
