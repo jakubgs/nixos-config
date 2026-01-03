fa77268135303aec5317a33492c38d9ed3476464
Jakub Sokołowski <jakub@status.im>
2026-01-03 13:51:56 +0100

    pkgs/erigon: upgrade from 3.3.0 to 3.3.2
    https://github.com/erigontech/erigon/releases/tag/v3.3.2
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/erigon.nix
+++ pkgs/erigon.nix
@@ -3,17 +3,17 @@
 # Can't use overrideAttrs due to how buildGoModule overwrites arguments.
 pkgs.erigon.override {
   buildGoModule = args: pkgs.buildGo124Module ( args // rec {
-    version = "3.3.0";
+    version = "3.3.2";
 
     src = pkgs.fetchFromGitHub {
       owner = "ledgerwatch";
       repo = args.pname;
       rev = "v${version}";
-      hash = "sha256-5h9TxOvEfDbOkVTW6Py97sQRfomgu7U5w+iZvuZLri8=";
+      hash = "sha256-jefTssNo1e0Bj/TxwQA4oxGKyJsVz0kZ5ncs/IjpswY=";
       fetchSubmodules = true;
     };
 
-    vendorHash = "sha256-J/cEUrw9U67+CZmb1woI2HltYyo2v90ap/jiOqwMesY=";
+    vendorHash = "sha256-i/ri6HDaF8Mz7UgO14TPR1GBAxnmYuvWDP/B0L5gRd8=";
 
     subPackages = [
       "cmd/erigon"
