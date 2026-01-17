70668d6947b769febf46f8929482ca588f4ba7b9
Jakub Sokołowski <jakub@status.im>
2026-01-17 21:23:49 +0100

    roles/desktop/network: add openvpn to networkmanager
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/desktop/network.nix
+++ roles/desktop/network.nix
@@ -5,7 +5,10 @@
   environment.systemPackages = with pkgs; [ nftables ];
 
   # Enable NetworkManager
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    plugins = [ pkgs.networkmanager-openvpn ];
+  };
 
   # Fix for Network Manager permission issues
   programs.dconf.enable = true;
