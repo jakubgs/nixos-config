b64e900a650121112bfa9c48590dc2a5545205e9
Jakub Sokołowski <jakub@status.im>
2026-02-21 23:08:25 +0100

    pkgs/go-ethereum: upgrade from 1.16.7 to 1.17.0
    https://github.com/ethereum/go-ethereum/releases/tag/v1.17.0
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/go-ethereum.nix
+++ pkgs/go-ethereum.nix
@@ -1,24 +1,19 @@
 { pkgs ? import <nixpkgs> { } }:
 
-# Can't use overrideAttrs due to how buildGoModule overwrites arguments.
-pkgs.go-ethereum.override {
-  buildGoModule = args: pkgs.buildGo124Module ( args // rec {
-    version = "1.16.7";
+pkgs.go-ethereum.overrideAttrs (args: rec {
+  version = "1.17.0";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "ethereum";
-      repo = args.pname;
-      rev = "v${version}";
-      sha256 = "sha256-aAkF8WqYml359kTCWyfLH7yo1aIOGY31rZWEFFPyKgI=";
-    };
+  src = pkgs.fetchFromGitHub {
+    owner = "ethereum";
+    repo = args.pname;
+    rev = "v${version}";
+    sha256 = "sha256-xTx2gcpDY4xuZOuUEmtV6m5NNO6YQ01tGzLr5rh9F/g=";
+  };
 
-    vendorHash = "sha256-KP9oD87kn8MCvEf3ply8HbP8xIBlGAEtthGob8Yh++A=";
+  vendorHash = "sha256-egsqYaItRtKe97P3SDb6+7sbuvyGdNGIwCR6V2lgGOc=";
 
-    subPackages = [
-      "cmd/ethkey"
-      "cmd/clef"
-      "cmd/geth"
-      "cmd/utils"
-    ];
-  });
-}
+  subPackages = [
+    "cmd/clef"
+    "cmd/geth"
+  ];
+})
