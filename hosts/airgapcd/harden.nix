8596986eec8e5ac74949d2391ae3270a5f359382
Jakub Sokołowski <jakub@status.im>
2026-02-24 12:35:22 +0100

    hosts/airgapcd: improve hardening of system
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- hosts/airgapcd/harden.nix
+++ hosts/airgapcd/harden.nix
@@ -1,6 +1,19 @@
 { lib, ... }:
 
 {
+  # Run entirely from RAM, quieter logs.
+  boot.kernelParams = [
+    "copytoram"
+    "quiet"
+    "loglevel=3"
+    "init_on_alloc=1"
+    "init_on_free=1"
+    "vsyscall=none"
+  ];
+
+  # Use improved randomness generator.
+  services.jitterentropy-rngd.enable = true;
+
   # Disable all suspend methods.
   systemd.targets.sleep.enable = false;
   systemd.targets.suspend.enable = false;
