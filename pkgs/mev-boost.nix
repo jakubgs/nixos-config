fd60fe0a1386def899b80663b473ba23d83dbb82
Jakub Sokołowski <jakub@status.im>
2026-03-16 19:48:11 +0100

    pkgs/mev-boost: upgrade from 1.11 to 1.12
    https://github.com/flashbots/mev-boost/releases/tag/v1.12
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- pkgs/mev-boost.nix
+++ pkgs/mev-boost.nix
@@ -2,13 +2,13 @@
 
 pkgs.buildGo124Module rec {
   pname = "mev-boost";
-  version = "1.11";
+  version = "1.12";
 
   src = pkgs.fetchFromGitHub {
     owner = "flashbots";
     repo = "mev-boost";
     rev = "v${version}";
-    hash = "sha256-uEIZojmzSVyF+ZOQsSqZA0MB2cT8I/JHGfgKVI48PIk=";
+    hash = "sha256-ZGeY3iNsOdAso7aMv0XMm1x2EhR8lz27joGIsz40Ibc=";
   };
 
   vendorHash = "sha256-dIc0ZHTx+7P621FvfDKlItc/FazUpwxRmDQF2SNVIwA=";
