2b1b45fb6e5071ace32c46f7001d35b6e2c2dd35
Jakub Sokołowski <jakub@status.im>
2026-07-26 15:37:11 +0200

    roles/ethereum/mev-boost: update relays list
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- roles/ethereum/mev-boost.nix
+++ roles/ethereum/mev-boost.nix
@@ -9,10 +9,9 @@
     logLevel = "info";
     package = unstablePkgs.mev-boost;
     relays = [
-      "https://0xac6e77dfe25ecd6110b8e780608cce0dab71fdd5ebea22a16c0205200f2f8e2e3ad3b71d3499c54ad14d6c21b41a37ae@boost-relay.flashbots.net"
-      "https://0xa00001792da52b2ad6606033396337f86e7598c8e0927d640bb649e0a9fe230df41f8ffd3403a5cd4fffd5131e2fbe47@rsync-builder.xyz"
+      "https://0xa1559ace749633b997cb3fdacffb890aeebdb0f5a3b6aaa7eeeaf1a38af0a8fe88b9e4b1f61f236d2e64d95733327a62@relay.ultrasound.money"
+      "https://0x8b5d2e73e2a3a55c6c87b8b6eb92e0149a125c852751db1422fa951e42a09b82c142c3ea98d0d9930b056a3bc9896b8f@bloxroute.max-profit.blxrbdn.com"
       "https://0x8c4ed5e24fe5c6ae21018437bde147693f68cda427cd1122cf20819c30eda7ed74f72dece09bb313f2a1855595ab677d@global.titanrelay.xyz"
-      "https://0xa7ab7a996c8584251c8f925da3170bdfd6ebc75d50f5ddc4050a6fdc77f2a3b5fce2cc750d0865e05d7228af97d69561@agnostic-relay.net"
     ];
   };
 }
