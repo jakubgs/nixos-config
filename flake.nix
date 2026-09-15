4a1e8a9cd1fbad9d45bb1ce7581c531583242137
Jakub Sokołowski <jakub@status.im>
2026-09-15 13:55:28 +0200

    flake: bump nimbus-eth1 to v0.4.1 release
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       inputs.home-manager.follows = "";
     };
     nimbus-eth1 = {
-      url = "git+https://github.com/status-im/nimbus-eth1?submodules=1&ref=nix/add-service-module";
+      url = "git+https://github.com/status-im/nimbus-eth1?submodules=1&ref=refs/tags/v0.4.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nimbus-eth2 = {
